43faf325b4d32345adda00f4a76a8e6c124eb15c
yu-lang.chu <yu-lang.chu@redheads.de>
2018-09-25 20:51:03 +0200

    some test

--- test/main.go
+++ test/main.go
@@ -1,17 +1,32 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 
 	issue := Issue{1, "test"}
-	issues := make([]Issue, 2)
-	issueSearchResult := IssueSearchResult{1, new}
-	issueSearchResult.Items = &issues
+	issues := make([]*Issue, 0)
+	issues = append(issues, &issue)
+
+	var items []Issue
+	items = append(items, issue)
+	fmt.Println(issue)
+	fmt.Println(issues)
+	fmt.Println(items)
+
+	issueSearchResult := IssueSearchResult{1, nil, nil}
+	issueSearchResult.ItemPointers = issues
+
+	fmt.Println(cap(issues))
 
 }
 
 type IssueSearchResult struct {
-	TotalCount int `json:"total_count"`
-	Items      []*Issue
+	TotalCount   int `json:"total_count"`
+	ItemPointers []*Issue
+	Items        []Issue
 }
 
 type Issue struct {
