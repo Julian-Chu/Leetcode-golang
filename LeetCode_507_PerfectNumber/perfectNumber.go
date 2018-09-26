4df2b129b44d7a87cbdc90e88b77023fb3ea48d7
JulianChu <yulang.chu@gmail.com>
2018-09-27 00:10:24 +0200

    fix if i == 1

--- LeetCode_507_PerfectNumber/perfectNumber.go
+++ LeetCode_507_PerfectNumber/perfectNumber.go
@@ -1,5 +1,36 @@
 package perfectNumber
 
+import (
+	"fmt"
+)
+
 func checkPerfectNumber(num int) bool {
-	return true
+	if num%2 != 0 {
+		return false
+	}
+
+	sum := 0
+	i := num / 2
+	for i > 0 {
+		if i == 1 {
+			sum++
+			i--
+		} else if num%i == 0 {
+			sum += i
+			if i%2 == 0 {
+				i = i / 2
+			} else {
+				i = (i + 1) / 2
+			}
+		} else {
+			i--
+		}
+	}
+
+	fmt.Println(sum)
+	if sum == num {
+		return true
+	} else {
+		return false
+	}
 }
