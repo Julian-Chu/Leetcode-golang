ae949ce95657c57ef3da4861949d4b1da8eaa212
yu-lang.chu <yu-lang.chu@redheads.de>
2018-09-27 13:09:08 +0200

    optimize

--- LeetCode_507_PerfectNumber/perfectNumber.go
+++ LeetCode_507_PerfectNumber/perfectNumber.go
@@ -1,31 +1,50 @@
 package perfectNumber
 
+import (
+	"math"
+)
+
+// func checkPerfectNumber(num int) bool {
+// 	if num%2 != 0 || num == 0 {
+// 		return false
+// 	}
+
+// 	sum := 0
+// 	i := num / 2
+// 	for i > 0 {
+// 		if i == 1 {
+// 			sum++
+// 			i--
+// 		} else if num%i == 0 {
+// 			sum += i
+// 			if i%2 == 0 {
+// 				i = i / 2
+// 			} else {
+// 				i = (i + 1) / 2
+// 			}
+// 		} else {
+// 			i--
+// 		}
+// 	}
+
+// 	if sum == num {
+// 		return true
+// 	} else {
+// 		return false
+// 	}
+// }
+
 func checkPerfectNumber(num int) bool {
-	if num%2 != 0 || num == 0 {
+	if num == 1 {
 		return false
 	}
+	root := int(math.Sqrt(float64(num)))
 
-	sum := 0
-	i := num / 2
-	for i > 0 {
-		if i == 1 {
-			sum++
-			i--
-		} else if num%i == 0 {
-			sum += i
-			if i%2 == 0 {
-				i = i / 2
-			} else {
-				i = (i + 1) / 2
-			}
-		} else {
-			i--
+	sum := 1
+	for i := 2; i <= root; i++ {
+		if num%i == 0 {
+			sum = sum + i + num/i
 		}
 	}
-
-	if sum == num {
-		return true
-	} else {
-		return false
-	}
+	return sum == num
 }
