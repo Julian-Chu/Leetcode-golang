eb4847954ec38492553ae61068674411562adee8
JulianChu <yulang.chu@gmail.com>
2018-09-28 23:59:10 +0200

    3rd optimize

--- LeetCode_661_ImageSmoother/main.go
+++ LeetCode_661_ImageSmoother/main.go
@@ -19,39 +19,24 @@ func imageSmoother(M [][]int) [][]int {
 	rowLength := len(M)
 	colLength := len(M[0])
 	res := make([][]int, rowLength)
-	for index := range res {
-		res[index] = make([]int, colLength)
-	}
-
-	for rowIndex, row := range M {
-		for colIndex := range row {
-			rowBegin := rowIndex - 1
-			if rowIndex == 0 {
-				rowBegin = 0
-			}
-			rowEnd := rowIndex + 1
-			if rowIndex == rowLength-1 {
-				rowEnd = rowIndex
-			}
-			colBegin := colIndex - 1
-			if colIndex == 0 {
-				colBegin = 0
-			}
-			colEnd := colIndex + 1
-			if colIndex == colLength-1 {
-				colEnd = colIndex
-			}
+	for i := range res {
+		res[i] = make([]int, colLength)
+		for j := range res[i] {
 			count := 0
 			sum := 0
-			for i := rowBegin; i <= rowEnd; i++ {
-				for j := colBegin; j <= colEnd; j++ {
-					count++
-					sum += M[i][j]
+			for x := i - 1; x <= i+1; x++ {
+				for y := j - 1; y <= j+1; y++ {
+					if x >= 0 && x < rowLength && y >= 0 && y < colLength {
+						count++
+						sum += M[x][y]
+					}
 				}
 			}
+
 			result := int(math.Floor(float64(sum / count)))
-			res[rowIndex][colIndex] = result
+			res[i][j] = result
 		}
 	}
+
 	return res
 }
