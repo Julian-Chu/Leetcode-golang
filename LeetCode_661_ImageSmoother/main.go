817df074f1cc458a63b631935a795eb6392bdf07
JulianChu <yulang.chu@gmail.com>
2018-10-01 23:20:42 +0200

    add benchmark test

--- LeetCode_661_ImageSmoother/main.go
+++ LeetCode_661_ImageSmoother/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
@@ -29,7 +30,6 @@ func imageSmoother(M [][]int) [][]int {
 }
 
 func calcAvg(M [][]int, i, j int) int {
-
 	rowLength := len(M)
 	colLength := len(M[0])
 	count := 0
@@ -44,4 +44,46 @@ func calcAvg(M [][]int, i, j int) int {
 	}
 
 	return sum / count
+}
+
+func imageSmootherWithoutSubfunc(M [][]int) [][]int {
+	rowLength := len(M)
+	colLength := len(M[0])
+	res := make([][]int, rowLength)
+	for index := range res {
+		res[index] = make([]int, colLength)
+	}
+
+	for rowIndex, row := range M {
+		for colIndex := range row {
+			rowBegin := rowIndex - 1
+			if rowIndex == 0 {
+				rowBegin = 0
+			}
+			rowEnd := rowIndex + 1
+			if rowIndex == rowLength-1 {
+				rowEnd = rowIndex
+			}
+			colBegin := colIndex - 1
+			if colIndex == 0 {
+				colBegin = 0
+			}
+			colEnd := colIndex + 1
+			if colIndex == colLength-1 {
+				colEnd = colIndex
+			}
+			count := 0
+			sum := 0
+			for i := rowBegin; i <= rowEnd; i++ {
+				for j := colBegin; j <= colEnd; j++ {
+					count++
+					sum += M[i][j]
+				}
+			}
+			result := int(math.Floor(float64(sum / count)))
+			res[rowIndex][colIndex] = result
+		}
+	}
+	return res
+
 }
---
+++ LeetCode_661_ImageSmoother/main2_test.go
@@ -0,0 +1,13 @@
+package main
+
+// func BenchmarkImageSmootherWithoutSubfunc(b *testing.B) {
+// 	matrix := [][]int{
+// 		{0, 1},
+// 		{1, 1},
+// 	}
+// 	b.ResetTimer()
+// 	for i := 0; i < b.N; i++ {
+// 		res := imageSmootherWithoutSubfunc(matrix)
+// 		fmt.Println(res)
+// 	}
+// }
