e78bf92f23556deb45787697a495b41970967f94
yu-lang.chu <yu-lang.chu@redheads.de>
2018-10-30 15:12:32 +0100

    2nd test case pass

--- LeetCode_56_MergeIntervals/mergeIntervals_test.go
+++ LeetCode_56_MergeIntervals/mergeIntervals_test.go
@@ -1,6 +1,7 @@
 package merge_intervals
 
 import (
+	"errors"
 	"fmt"
 	"reflect"
 	"testing"
@@ -20,15 +21,53 @@ type Interval struct {
 }
 
 func merge(intervals []Interval) []Interval {
-	return []Interval{Interval{1, 5}}
+	res := make([]Interval, 0)
+	for _, interval := range intervals {
+		if len(res) == 0 {
+			res = append(res, interval)
+		}
+
+		compareObj := res[len(res)-1]
+		fmt.Println(compareObj)
+		mergedInv, err := mergeInterval(compareObj, interval)
+		if err == nil {
+			res[len(res)-1] = mergedInv
+			continue
+		}
+		res = append(res, interval)
+	}
+	return res
 }
 
+func mergeInterval(src Interval, dst Interval) (Interval, error) {
+	if src.End < dst.Start {
+		return Interval{}, errors.New("no merge")
+	}
+	var newStart, newEnd int
+	newStart = dst.Start
+	if src.Start <= dst.Start {
+		newStart = src.Start
+	}
+	newEnd = dst.End
+	if src.End >= dst.End {
+		newEnd = src.End
+	}
+	return Interval{newStart, newEnd}, nil
+
+}
 func Test_case1(t *testing.T) {
 	input := []Interval{Interval{1, 4}, Interval{4, 5}}
 	res := merge(input)
-	fmt.Println(res)
 	if !reflect.DeepEqual(res, []Interval{Interval{1, 5}}) {
 		t.Error("Failed")
 	}
+}
 
+func Test_case2(t *testing.T) {
+	input := []Interval{Interval{1, 3}, Interval{4, 5}}
+	res := merge(input)
+	fmt.Println(res)
+	if !reflect.DeepEqual(res, []Interval{Interval{1, 3}, Interval{4, 5}}) {
+		t.Error("Failed")
+	}
 }
