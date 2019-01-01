0431834d998ec3939aaa95cdadacf0e87d8908d6
JulianChu <yulang.chu@gmail.com>
2019-01-01 15:10:19 +0100

    137 pass

--- LeetCode_137_SingleNumberII/main_test.go
+++ LeetCode_137_SingleNumberII/main_test.go
@@ -3,7 +3,21 @@ package LeetCode_137_SingleNumberII
 import "testing"
 
 func singleNumber(nums []int) int {
-	return 0
+	singleNumber := 0
+	for i := 0; i < 32; i++ {
+
+		bit := 1 << uint(i)
+		count := 0
+		for j := 0; j < len(nums); j++ {
+			if nums[j]&bit != 0 {
+				count++
+			}
+		}
+		if count%3 != 0 {
+			singleNumber |= bit
+		}
+	}
+	return singleNumber
 }
 
 var testcases = []struct {
@@ -11,6 +25,7 @@ var testcases = []struct {
 	output int
 }{
 	{input: []int{2, 2, 3, 2}, output: 3},
+	{input: []int{0, 1, 0, 1, 0, 1, 99}, output: 99},
 }
 
 func Test_Cases(t *testing.T) {
@@ -20,4 +35,10 @@ func Test_Cases(t *testing.T) {
 		}
 	}
 
+}
+
+func Benchmark_SingleNumberII(b *testing.B) {
+	for _, c := range testcases {
+		singleNumber(c.input)
+	}
 }
