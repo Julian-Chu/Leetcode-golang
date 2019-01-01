137a72138679cd86d74d94e8864d3c9a3340fabe
JulianChu <yulang.chu@gmail.com>
2019-01-01 13:01:18 +0100

    add second solution to 136

--- LeetCode_136_SingleNumber/main_test.go
+++ LeetCode_136_SingleNumber/main_test.go
@@ -1,6 +1,8 @@
 package LeetCode_136_SingleNumber
 
-import "testing"
+import (
+	"testing"
+)
 
 func singleNumber(nums []int) int {
 	xor := 0
@@ -8,12 +10,32 @@ func singleNumber(nums []int) int {
 		xor ^= n
 	}
 	return xor
+
+	//ans := 0
+	//var i uint
+
+	//	count := 0
+	//	bit := 1 << i
+	//	for j := 0; j < len(nums); j++ {
+	//		if nums[j]&bit != 0 {
+	//			count++
+	//		}
+	//	}
+	//	if count%2 != 0 {
+	//		ans |= bit
+	//	}
+	//}
+	//return ans
 }
 
 var cases = []struct {
 	numbers []int
 	num     int
 }{
+	{
+		[]int{1},
+		1,
+	},
 	{
 		[]int{2, 2, 1},
 		1,
@@ -30,5 +52,12 @@ func TestCases(t *testing.T) {
 			t.Error("Failed")
 		}
 	}
+}
 
+func BenchmarkSingleNumber(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, c := range cases {
+			singleNumber(c.numbers)
+		}
+	}
 }
