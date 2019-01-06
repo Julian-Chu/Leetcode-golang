2d795fa2ee828b76f924ef5f824ce866503e17e7
JulianChu <yulang.chu@gmail.com>
2019-01-06 22:56:47 +0100

    add 83

---
+++ LeetCode_83_RemoveDuplicatesFromSortedList/removeDuplicatesFromSortedList.go
@@ -0,0 +1,33 @@
+package LeetCode_83_RemoveDuplicatesFromSortedList
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	mapper := map[int]bool{}
+	var pre, cur *ListNode
+	cur = head
+	for cur != nil {
+		if mapper[cur.Val] == true {
+			cur = cur.Next
+		} else {
+			mapper[cur.Val] = true
+			if pre != nil {
+				pre.Next = cur
+			}
+			pre = cur
+			cur = pre.Next
+		}
+	}
+
+	return head
+}
