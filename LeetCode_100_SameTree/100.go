4896f1699377c84888f7947bbb526bfee9f1b7cb
yu-lang.chu <yu-lang.chu@redheads.de>
2019-01-17 12:05:22 +0100

    pass

--- LeetCode_100_SameTree/100.go
+++ LeetCode_100_SameTree/100.go
@@ -1,7 +1,19 @@
 package leetcode100
 
 func isSameTree(p *TreeNode, q *TreeNode) bool {
-	return true
+	if p == nil {
+		return q == nil
+	}
+
+	if q == nil {
+		return false
+	}
+
+	if p.Val != q.Val {
+		return false
+	}
+
+	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
 }
 
 type TreeNode struct {
