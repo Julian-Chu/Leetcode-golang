033d23e6afdf0f28ec71334909e424e574feaa5a
yu-lang.chu <yu-lang.chu@redheads.de>
2019-01-17 11:41:25 +0100

    add leetcode 100

---
+++ LeetCode_100_SameTree/100.go
@@ -0,0 +1,11 @@
+package leetcode100
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	return true
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
---
+++ LeetCode_100_SameTree/100_test.go
@@ -0,0 +1,57 @@
+package leetcode100
+
+import "testing"
+
+func Test_isSameTree(t *testing.T) {
+	type args struct {
+		p *TreeNode
+		q *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"Case1", args{
+			p: &TreeNode{
+				Val: 1,
+				Left: &TreeNode{
+					Val: 2,
+				},
+				Right: &TreeNode{
+					Val: 3,
+				},
+			},
+			q: &TreeNode{
+				Val: 1,
+				Left: &TreeNode{
+					Val: 2,
+				},
+				Right: &TreeNode{
+					Val: 3,
+				},
+			},
+		}, true},
+		{"Case2", args{
+			p: &TreeNode{
+				Val: 1,
+				Left: &TreeNode{
+					Val: 2,
+				},
+			},
+			q: &TreeNode{
+				Val: 1,
+				Right: &TreeNode{
+					Val: 2,
+				},
+			},
+		}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isSameTree(tt.args.p, tt.args.q); got != tt.want {
+				t.Errorf("isSameTree() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
