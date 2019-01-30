5ccaefe476ef043bd380c5e987a561a1f35a5950
yu-lang.chu <yu-lang.chu@redheads.de>
2019-01-30 14:50:24 +0100

    add 897

---
+++ LeetCode_897_IncreasingOrderSearchTree/897.go
@@ -0,0 +1,16 @@
+package leetcode897
+
+import . "Leetcode-golang/helper"
+
+func increasingBST(root *TreeNode) *TreeNode {
+
+	return traverse(root)
+}
+
+func traverse(root *TreeNode) *TreeNode {
+
+	if root.Left !=nil{
+		
+	}
+
+}
