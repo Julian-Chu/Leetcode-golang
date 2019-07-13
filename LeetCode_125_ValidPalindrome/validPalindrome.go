6040ba974837de9585c0c32dd11423590453b128
JulianChu <yulang.chu@gmail.com>
2019-07-13 23:00:20 +0200

    optimize 125

--- LeetCode_125_ValidPalindrome/validPalindrome.go
+++ LeetCode_125_ValidPalindrome/validPalindrome.go
@@ -1,41 +1,30 @@
 package LeetCode_125_ValidPalindrome
 
 import (
-	"fmt"
 	"strings"
 )
 
 func isPalindrome(s string) bool {
-	var r []rune
-	for _, v := range []rune(strings.ToLower(s)) {
-		if isAlphbanumeric(v) {
-			r = append(r, v)
+	s = strings.ToLower(s)
+	i, j := 0, len(s)-1
+	for i < j {
+		for i < j && !isAlphbanumeric(s[i]) {
+			i++
 		}
-	}
-	if r == nil {
-		return true
-	}
-
-	fmt.Println(r)
-	l := len(r)
-	mid := l / 2
-	b, e := mid, mid
-	if l%2 == 0 {
-		b, e = mid-1, mid
-	}
-
-	for b >= 0 && e < l {
-		if r[b] != r[e] {
+		for i < j && !isAlphbanumeric(s[j]) {
+			j--
+		}
+		if s[i] != s[j] {
 			return false
 		}
-		b--
-		e++
+		i++
+		j--
 	}
 	return true
 }
 
-func isAlphbanumeric(r rune) bool {
-	if 48 <= r && r <= 57 || 65 <= r && r <= 90 || 97 <= r && r <= 122 {
+func isAlphbanumeric(c byte) bool {
+	if (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') {
 		return true
 	}
 	return false
