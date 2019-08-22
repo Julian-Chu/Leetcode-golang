617e5b1e2768ef91ca8b373bc2d2fb00fd863c8c
yu-lang.chu <yu-lang.chu@redheads.de>
2019-08-22 18:51:22 +0200

    1143

--- LeetCode_1143_LongestCommonSubsequence/1143_test.go
+++ LeetCode_1143_LongestCommonSubsequence/1143_test.go
@@ -13,12 +13,93 @@ func Test_longestCommonSubsequence(t *testing.T) {
 		want int
 	}{
 		{
+			name: "a a",
+			args: args{
+				"a",
+				"a",
+			},
+			want: 1,
+		},
+		{
+			name: "ab a",
+			args: args{
+				"ab",
+				"a",
+			},
+			want: 1,
+		},
+		{
+			name: "ab ac",
+			args: args{
+				"ab",
+				"ac",
+			},
+			want: 1,
+		},
+		{
+			name: "abc ac",
+			args: args{
+				"abc",
+				"ac",
+			},
+			want: 2,
+		},
+		{
 			name: "abcde ace",
 			args: args{
 				"abcde",
 				"ace",
 			},
 			want: 3,
+		},
+
+		{
+			name: "abc def",
+			args: args{
+				"abc",
+				"def",
+			},
+			want: 0,
+		}, {
+			name: "bsbininm jmjkbkjkv",
+			args: args{
+				"bsbininm",
+				"jmjkbkjkv",
+			},
+			want: 1,
+		},
+
+		{
+			name: "ezupkr ubmrapg",
+			args: args{
+				"ezupkr",
+				"ubmrapg",
+			},
+			want: 2,
+		},
+		{
+			name: "oxcpqrsvwf shmtulqrypy",
+			args: args{
+				"oxcpqrsvwf",
+				"shmtulqrypy",
+			},
+			want: 2,
+		},
+		{
+			name: "pmjghexybyrgzczy hafcdqbgncrcbihkd",
+			args: args{
+				"pmjghexybyrgzczy",
+				"hafcdqbgncrcbihkd",
+			},
+			want: 4,
+		},
+		{
+			name: "mhunuzqrkzsnidwbun szulspmhwpazoxijwbq",
+			args: args{
+				"mhunuzqrkzsnidwbun",
+				"szulspmhwpazoxijwbq",
+			},
+			want: 6,
 		},
 	}
 	for _, tt := range tests {
