fb09e44dca25d00d1e814b8ba4fd49407c6a4c21
yu-lang.chu <yu-lang.chu@redheads.de>
2019-08-26 11:54:51 +0200

    opt

--- LeetCode_583_DeleteOperationForTwoStrings/583.go
+++ LeetCode_583_DeleteOperationForTwoStrings/583.go
@@ -1,41 +1,21 @@
 package leetcode583
 
-import (
-	"strings"
-)
-
 func minDistance(word1 string, word2 string) int {
-	if strings.Compare(word1, word2) == 0 {
-		return 0
-	}
-	res := make([][]int, len(word1))
-	maxLen := 0
+	n1, n2 := len(word1), len(word2)
+	res := make([][]int, n1+1)
 	for i := range res {
-		res[i] = make([]int, len(word2))
-		for j := range res[i] {
-			if word1[i] == word2[j] {
-				res[i][j] = 1
-				if i > 0 && j > 0 {
-					res[i][j] = max(1, res[i-1][j-1]+1)
-				}
-				if maxLen < res[i][j] {
-					maxLen = res[i][j]
-				}
-				continue
-			}
-			if i > 0 && j > 0 {
-				res[i][j] = max(res[i-1][j], res[i][j-1])
-			} else if i > 0 && j == 0 {
-				res[i][j] = res[i-1][j]
-			} else if i == 0 && j > 0 {
-				res[i][j] = res[i][j-1]
-			}
-			if maxLen < res[i][j] {
-				maxLen = res[i][j]
+		res[i] = make([]int, n2+1)
+	}
+
+	for i := 1; i <= n1; i++ {
+		for j := 1; j <= n2; j++ {
+			res[i][j] = max(res[i-1][j], res[i][j-1])
+			if word1[i-1] == word2[j-1] {
+				res[i][j] = res[i-1][j-1] + 1
 			}
 		}
 	}
-	return len(word1) + len(word2) - maxLen*2
+	return n1 + n2 - res[n1][n2]*2
 }
 
 func max(a, b int) int {
