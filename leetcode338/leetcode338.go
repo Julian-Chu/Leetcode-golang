2851693b68ed43440916e1b7a23b8de6876c13ec
Julian Chu <yulang.chu@gmail.com>
2019-09-09 17:22:08 +0200

    add leetcode338

--- leetcode338/leetcode338.go
+++ leetcode338/leetcode338.go
@@ -3,14 +3,23 @@ package leetcode338
 func countBits(num int) []int {
 	res := make([]int, num+1)
 
-	for i := 0; i <= num; i++ {
-		n := i
-		for n > 0 {
-			if n&1 == 1 {
-				res[i]++
-			}
-			n = n >> 1
-		}
+	for i := 1; i <= num; i++ {
+		res[i] = res[i>>1] + i&1
 	}
 	return res
 }
+
+//func countBits(num int) []int {
+//	res := make([]int, num+1)
+//
+//	for i := 0; i <= num; i++ {
+//		n := i
+//		for n > 0 {
+//			if n&1 == 1 {
+//				res[i]++
+//			}
+//			n = n >> 1
+//		}
+//	}
+//	return res
+//}
