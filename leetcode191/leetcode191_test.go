30ecab60042f7b6ebe5e499cceb4ec7e7b295946
Julian Chu <yulang.chu@gmail.com>
2019-09-09 13:16:33 +0200

    add leetcode190

---
+++ leetcode191/leetcode191_test.go
@@ -0,0 +1,36 @@
+package leetcode191
+
+import "testing"
+
+func Test_hammingWeight(t *testing.T) {
+	type args struct {
+		num uint32
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "1",
+			args: args{
+				num: 1,
+			},
+			want: 1,
+		},
+		{
+			name: "5",
+			args: args{
+				num: 5,
+			},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := hammingWeight(tt.args.num); got != tt.want {
+				t.Errorf("hammingWeight() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
