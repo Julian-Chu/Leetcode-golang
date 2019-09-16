6c6b007536861d35f54d726a8eb89696665f74fc
yu-lang.chu <yu-lang.chu@redheads.de>
2019-09-16 16:21:22 +0200

    add leetcode47(done)

---
+++ leetcode47/leetcode47_test.go
@@ -0,0 +1,53 @@
+package leetcode47
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_permuteUnique(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			name: "[1,1,1]",
+			args: args{
+				nums: []int{1, 1, 1},
+			},
+			want: [][]int{{1, 1, 1}},
+		},
+		{
+			name: "",
+			args: args{
+				nums: []int{1, 2, 1, 2},
+			},
+			want: [][]int{{1, 1, 2, 2}, {1, 2, 1, 2}, {1, 2, 2, 1}, {2, 1, 1, 2}, {2, 1, 2, 1}, {2, 2, 1, 1}},
+		},
+		{
+			name: "",
+			args: args{
+				nums: []int{1, 1, 1, 3},
+			},
+			want: [][]int{{1, 1, 1, 3}, {1, 1, 3, 1}, {1, 3, 1, 1}, {3, 1, 1, 1}},
+		},
+		{
+			name: "",
+			args: args{
+				nums: []int{1, 1, 1, 3, 3},
+			},
+			want: [][]int{{1, 1, 1, 3, 3}, {1, 1, 3, 1, 3}, {1, 3, 1, 1, 3}, {1, 1, 3, 3, 1}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := permuteUnique(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("permuteUnique() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
