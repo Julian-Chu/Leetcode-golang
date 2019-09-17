4fb5818dbb1a8dccd46e886faea094c8266d5e97
Julian Chu <yulang.chu@gmail.com>
2019-09-17 23:58:51 +0200

    add leetcode60(not done)

---
+++ leetcode60/leetcode60.go
@@ -0,0 +1,38 @@
+package leetcode60
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func getPermutation(n int, k int) string {
+	res := make([]string, 0)
+	nums := make([]int, n)
+	for i := 1; i <= n; i++ {
+		nums[i-1] = i
+	}
+
+	recur([]int{}, nums, &res)
+	return res[k-1]
+}
+
+func recur(solution, nums []int, res *[]string) {
+	if len(nums) == 0 {
+		ret := ""
+		fmt.Printf("%v", solution)
+		for _, v := range solution {
+			ret += strconv.Itoa(v)
+		}
+		*res = append(*res, ret)
+		return
+	}
+	solution = solution[:len(solution):len(solution)]
+	for i := 0; i < len(nums); i++ {
+		if i == 0 {
+			recur(append(solution, nums[0]), append([]int{}, nums[1:]...), res)
+			continue
+		}
+
+		recur(append(solution, nums[i]), append(append([]int{}, nums[0:i]...), nums[i+1:]...), res)
+	}
+}
---
+++ leetcode60/leetcode60_test.go
@@ -0,0 +1,31 @@
+package leetcode60
+
+import "testing"
+
+func Test_getPermutation(t *testing.T) {
+	type args struct {
+		n int
+		k int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "[1,2]",
+			args: args{
+				n: 2,
+				k: 1,
+			},
+			want: "12",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getPermutation(tt.args.n, tt.args.k); got != tt.want {
+				t.Errorf("getPermutation() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
