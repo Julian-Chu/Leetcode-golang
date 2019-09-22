e2531e5853b6778b66b7207f74546c0ca8db3b45
Julian Chu <yulang.chu@gmail.com>
2019-09-22 22:52:59 +0200

    add leetcode78

--- leetcode78/leetcode78.go
+++ leetcode78/leetcode78.go
@@ -1,32 +1,13 @@
 package leetcode78
 
-import (
-	"sort"
-)
-
 func subsets(nums []int) [][]int {
-	sort.Ints(nums)
-	res := make([][]int, 0)
-	res = append(res, []int{})
-	if len(nums) == 0 {
-		return res
+	res := make([][]int, 1, 1024)
+
+	for _, n := range nums {
+		for _, r := range res {
+			res = append(res, append([]int{n}, r...))
+		}
 	}
 
-	var dfs func(int)
-	dfs = func(i int) {
-		if i == len(nums) {
-			return
-		}
-		newSolutions := make([][]int, 0)
-		for j := range res {
-			res[j] = res[j][:len(res[j]):len(res[j])]
-			next := append(res[j], nums[i])
-			newSolutions = append(newSolutions, next)
-		}
-
-		res = append(res, newSolutions...)
-		dfs(i + 1)
-	}
-	dfs(0)
 	return res
 }
