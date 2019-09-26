fa15505b0d1fea5e1ffc9dcba618c1bdfcbbb819
yu-lang.chu <yu-lang.chu@redheads.de>
2019-09-26 13:10:18 +0200

    add leetcode79(done,  refactor)

--- leetcode79/leetcode79.go
+++ leetcode79/leetcode79.go
@@ -1,45 +1,36 @@
 package leetcode79
 
 func exist(board [][]byte, word string) bool {
-	if len(board) == 0 {
+	if len(board) == 0 || len(board[0]) == 0 || len(word) == 0 {
 		return false
 	}
-	val := false
-	var dfs func(int, int, int)
-	dfs = func(r, c, index int) {
+	var dfs func(int, int, int) bool
+	dfs = func(r, c, index int) bool {
 		if index == len(word) {
-			val = true
-			return
+			return true
 		}
 
 		if r < 0 || c < 0 || r > len(board)-1 || c > len(board[0])-1 || board[r][c] != word[index] {
-			return
+			return false
 		}
 
 		temp := board[r][c]
 		board[r][c] = 0
-		dfs(r-1, c, index+1)
-		if val == true {
-			return
+		if dfs(r-1, c, index+1) || dfs(r+1, c, index+1) || dfs(r, c-1, index+1) || dfs(r, c+1, index+1) {
+			return true
 		}
-		dfs(r+1, c, index+1)
-		if val == true {
-			return
-		}
-		dfs(r, c-1, index+1)
-		if val == true {
-			return
-		}
-		dfs(r, c+1, index+1)
 		board[r][c] = temp
+		return false
 	}
 
 	for i := 0; i < len(board); i++ {
 		for j := 0; j < len(board[0]); j++ {
 			if board[i][j] == word[0] {
-				dfs(i, j, 0)
+				if dfs(i, j, 0) {
+					return true
+				}
 			}
 		}
 	}
-	return val
+	return false
 }
