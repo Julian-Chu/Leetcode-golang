5112f8bbd0f336c16841cd6553596662e1aaf973
Julian Chu <yulang.chu@gmail.com>
2019-09-28 23:19:32 +0200

    add leetcode90

--- leetcode90/leetcode90.go
+++ leetcode90/leetcode90.go
@@ -2,41 +2,21 @@ package leetcode90
 
 import (
 	"sort"
-	"strconv"
-	"strings"
 )
 
 func subsetsWithDup(nums []int) [][]int {
-	sort.Ints(nums)
 	res := make([][]int, 0)
-	mapper := make(map[string]bool)
-
-	if len(nums) == 0 {
-		res = append(res, []int{})
-		return res
-	}
+	sort.Ints(nums)
 
 	var dfs func(int, []int)
 
 	dfs = func(index int, solution []int) {
-		if index == len(nums) {
-			solutionStr := make([]string, len(solution))
-			for i := range solution {
-				solutionStr[i] += strconv.Itoa(solution[i])
-			}
-			key := strings.Join(solutionStr, "-")
-			if _, ok := mapper[key]; !ok {
-				mapper[key] = true
-				res = append(res, solution)
-			}
-			return
-		}
 		solution = solution[:len(solution):len(solution)]
+		res = append(res, solution)
 		for i := index; i < len(nums); i++ {
-			//if i > 0 && nums[i-1] != nums[i] {
-			dfs(i+1, solution)
-			//}
-			dfs(i+1, append(solution, nums[i]))
+			if i == index || nums[i] != nums[i-1] {
+				dfs(i+1, append(solution, nums[i]))
+			}
 		}
 	}
 
