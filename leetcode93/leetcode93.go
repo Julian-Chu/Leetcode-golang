abf5aa76b8ee4ea6247128d18c4774932b40d7a3
Julian Chu <yulang.chu@gmail.com>
2019-09-29 23:58:17 +0200

    add leetcode93(start)

---
+++ leetcode93/leetcode93.go
@@ -0,0 +1,7 @@
+package leetcode93
+
+func restoreIpAddresses(s string) []string {
+	// submask which starts with zero has only digit
+	// submask max 3 digits, less than or equal to 255
+	return []string{}
+}
