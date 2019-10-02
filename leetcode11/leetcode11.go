e1928d81a08b10fe6bfa9686e53a3805758a4ada
Julian Chu <yulang.chu@gmail.com>
2019-10-02 23:24:57 +0200

    add leetcode11

--- leetcode11/leetcode11.go
+++ leetcode11/leetcode11.go
@@ -2,16 +2,23 @@ package leetcode11
 
 func maxArea(height []int) int {
 	max := 0
-	for i := 0; i < len(height)-1; i++ {
-		for j := i + 1; j < len(height); j++ {
-			h := height[i]
-			if height[i] > height[j] {
-				h = height[j]
-			}
-			v := h * (j - i)
-			if v > max {
-				max = v
-			}
+	i, j := 0, len(height)-1
+
+	for i < j {
+		h := height[i]
+		if height[i] > height[j] {
+			h = height[j]
+		}
+
+		area := h * (j - i)
+		if max < area {
+			max = area
+		}
+
+		if height[i] < height[j] {
+			i++
+		} else {
+			j--
 		}
 	}
 	return max
