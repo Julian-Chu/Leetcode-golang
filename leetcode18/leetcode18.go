b9eeee4733801a9bb9d032553331ff0985ad7679
Julian Chu <yulang.chu@gmail.com>
2019-10-04 00:45:45 +0200

    add leetcode18

--- leetcode18/leetcode18.go
+++ leetcode18/leetcode18.go
@@ -3,8 +3,15 @@ package leetcode18
 import "sort"
 
 func fourSum(nums []int, target int) [][]int {
-	res := make([][]int, 0)
+	if len(nums) < 4 {
+		return [][]int{}
+	}
 	sort.Ints(nums)
+
+	if nums[0]*4 > target || nums[len(nums)-1]*4 < target {
+		return [][]int{}
+	}
+	res := make([][]int, 0)
 	for i := 0; i < len(nums)-3; i++ {
 		if i > 0 && nums[i] == nums[i-1] {
 			continue
@@ -20,8 +27,14 @@ func fourSum(nums []int, target int) [][]int {
 				switch {
 				case sum > target:
 					r--
+					for l < r && nums[r] == nums[r+1] {
+						r--
+					}
 				case sum < target:
 					l++
+					for l < r && nums[l] == nums[l-1] {
+						l++
+					}
 				default:
 					res = append(res, []int{nums[i], nums[j], nums[l], nums[r]})
 					l++
