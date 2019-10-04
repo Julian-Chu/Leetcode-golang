190e3449a02122a68fc9ec2d54e3ac18cf75a3c4
yu-lang.chu <yu-lang.chu@redheads.de>
2019-10-04 14:01:15 +0200

    add leetcode26

--- leetcode26/leetcode26.go
+++ leetcode26/leetcode26.go
@@ -1,13 +1,15 @@
 package leetcode26
 
 func removeDuplicates(nums []int) int {
-	l, r := 0, 1
-	for ; r < len(nums); r++ {
-		if nums[l] == nums[r] {
+	size := len(nums)
+	cur := 0
+	for i := 1; i < size; i++ {
+		if nums[cur] != nums[i] {
+			cur++
+			nums[cur] = nums[i]
 			continue
 		}
-		l++
-		nums[l], nums[r] = nums[r], nums[l]
+
 	}
-	return l + 1
+	return cur + 1
 }
--- leetcode26/leetcode26_test.go
+++ leetcode26/leetcode26_test.go
@@ -18,6 +18,13 @@ func Test_removeDuplicates(t *testing.T) {
 			},
 			want: 2,
 		},
+		{
+			name: "[1,1]",
+			args: args{
+				nums: []int{1, 1},
+			},
+			want: 1,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
