ee3c81b020ff1229f7f707618420864ab0640d53
Julian Chu <yulang.chu@gmail.com>
2019-10-05 20:01:29 +0200

    add leetcode33

--- leetcode33/leetcode33.go
+++ leetcode33/leetcode33.go
@@ -1,51 +1,29 @@
 package leetcode33
 
 func search(nums []int, target int) int {
-	if len(nums) <= 2 {
-		for i, v := range nums {
-			if v == target {
-				return i
-			}
-		}
-		return -1
-	}
 	l, r := 0, len(nums)-1
-	mid := (l + r) / 2
-
-	for mid > l && nums[mid] < nums[l] {
-		mid--
-	}
-
-	for mid < r && nums[mid+1] > nums[r] {
-		mid++
-	}
-
-	if mid == l {
-		if target > nums[l] || target < nums[l+1] {
-			return -1
+	for l < r {
+		mid := (l + r) / 2
+		if nums[mid] > nums[r] {
+			l = mid + 1
+		} else {
+			r = mid
 		}
 	}
+	offset := l
 
-	if target < nums[l] {
-		l = mid + 1
-	} else if target > nums[r] {
-		r = mid
-	}
-
-	mid = (l + r) / 2
-	for l < r {
-		if target > nums[mid] {
+	l, r = 0, len(nums)-1
+	for l <= r {
+		mid := (l + r) / 2
+		midOffset := (mid + offset) % len(nums)
+		if target > nums[midOffset] {
 			l = mid + 1
-		} else if target < nums[mid] {
+		} else if target < nums[midOffset] {
 			r = mid - 1
 		} else {
-			return mid
+			return midOffset
 		}
-		mid = (l + r) / 2
 	}
 
-	if nums[mid] != target {
-		return -1
-	}
-	return mid
+	return -1
 }
