7f702a0696710d9aac942690657065d95ef77238
Julian Chu <yulang.chu@gmail.com>
2019-10-09 20:58:02 +0200

    add leetcode54

---
+++ leetcode54/leetcode54_test.go
@@ -0,0 +1,60 @@
+package leetcode54
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_spiralOrder(t *testing.T) {
+	type args struct {
+		matrix [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "[[1,2],[3,4]]",
+			args: args{
+				matrix: [][]int{{1, 2}, {3, 4}},
+			},
+			want: []int{1, 2, 4, 3},
+		},
+		{
+			name: "[[1,2,3],[4,5,6],[7,8,9]]",
+			args: args{
+				matrix: [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}},
+			},
+			want: []int{1, 2, 3, 6, 9, 8, 7, 4, 5},
+		},
+		{
+			name: "[[1,2,3],[4,5,6]]",
+			args: args{
+				matrix: [][]int{{1, 2, 3}, {4, 5, 6}},
+			},
+			want: []int{1, 2, 3, 6, 5, 4},
+		},
+		{
+			name: "[[1,2,3,4],[10,11,12,13]]",
+			args: args{
+				matrix: [][]int{{1, 2, 3, 4}, {10, 11, 12, 13}},
+			},
+			want: []int{1, 2, 3, 4, 13, 12, 11, 10},
+		},
+		{
+			name: "[[1,2],[3,4],[5,6],[7,8]]",
+			args: args{
+				matrix: [][]int{{1, 2}, {3, 4}, {5, 6}, {7, 8}},
+			},
+			want: []int{1, 2, 4, 6, 8, 7, 5, 3},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := spiralOrder(tt.args.matrix); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("spiralOrder() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
