1dc712739052e1003d811bd44875a36bf295daaf
Julian Chu <yulang.chu@gmail.com>
2019-10-21 23:45:56 +0200

    add leetcode80

--- leetcode80/leetcode80.go
+++ leetcode80/leetcode80.go
@@ -2,52 +2,13 @@ package leetcode80
 
 func removeDuplicates(nums []int) int {
 	n := len(nums)
-	if len(nums) < 3 {
-		return n
-	}
 
-	if nums[0] == nums[n-1] {
-		return 2
-	}
-
-	var moveToTail func(int)
-	moveToTail = func(i int) {
-		if i == n-1 {
-			return
-		}
-
-		nums[i], nums[i+1] = nums[i+1], nums[i]
-		moveToTail(i + 1)
-	}
-
-	i := 0
-	cnt := 0
-	for i < n {
-		if i > 0 && nums[i] < nums[i-1] {
-			return i
-		}
-		if i < 2 {
+	i := 2
+	for j := i; j < n; j++ {
+		if nums[i-2] != nums[j] {
+			nums[i] = nums[j]
 			i++
-			continue
 		}
-
-		if (nums[i] == nums[i-1]) && (nums[i] == nums[i-2]) {
-			if i == n-1 {
-				return i
-			}
-			if cnt > n-1-i {
-				return i
-			}
-			moveToTail(i)
-			cnt++
-			continue
-		}
-		if i < n-1 && nums[i] > nums[i+1] {
-			return i + 1
-		}
-		cnt = 0
-		i++
 	}
-
-	return n
+	return i
 }
