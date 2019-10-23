6d37e4535348692085310525404aef289c3b969a
Julian Chu <yulang.chu@gmail.com>
2019-10-24 01:03:19 +0200

    add leetcode105(done)

--- leetcode105/leetcode105.go
+++ leetcode105/leetcode105.go
@@ -1,55 +1,28 @@
 package leetcode105
 
-/**
- * Definition for a binary tree node.
- * type TreeNode struct {
- *     Val int
- *     Left *TreeNode
- *     Right *TreeNode
- * }
- */
-func buildTree(preorder []int, inorder []int) *TreeNode {
-	root := &TreeNode{
-		Val:   preorder[0],
-		Left:  nil,
-		Right: nil,
-	}
-	mid := 0
-	for i := range inorder {
-		if inorder[i] == preorder[0] {
-			mid = i
-			break
-		}
-	}
-
-	build(root, preorder[1:], inorder[:mid], inorder[mid+1:])
-	return root
-}
-
-func build(root *TreeNode, preorder, inorderLeft, inorderRight []int) {
-	if len(inorderLeft) == 0 && len(inorderRight) == 0 {
-		return
-	}
-	for i := range inorderLeft {
-		if preorder[0] == inorderLeft[i] {
-			root.Left = &TreeNode{
-				Val: preorder[0],
-			}
-			build(root.Left, preorder[1:], inorderLeft[:i], inorderRight[i+1:])
-		}
-	}
-	for i := range inorderRight {
-		if preorder[1] == inorderRight[i] {
-			root.Right = &TreeNode{
-				Val: preorder[1],
-			}
-			build(root.Left, preorder[1:], inorderLeft[:i], inorderRight[i+1:])
-		}
-	}
-}
-
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
 	Right *TreeNode
+}
+
+func buildTree(preorder []int, inorder []int) *TreeNode {
+	if len(preorder) == 0 {
+		return nil
+	}
+	root := &TreeNode{
+		Val: preorder[0],
+	}
+
+	idx := 0
+	for i := range inorder {
+		if inorder[i] == preorder[0] {
+			idx = i
+			break
+		}
+	}
+
+	root.Left = buildTree(preorder[1:idx+1], inorder[:idx])
+	root.Right = buildTree(preorder[idx+1:], inorder[idx+1:])
+	return root
 }
