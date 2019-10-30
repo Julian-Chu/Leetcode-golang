b8452c464116b0a01dd167fff2ef0876cfac3b95
Julian Chu <yulang.chu@gmail.com>
2019-10-30 20:39:51 +0100

    add leetcode122

--- leetcode122/leetcodd122.go
+++ leetcode122/leetcodd122.go
@@ -1,29 +1,11 @@
 package leetcode122
 
 func maxProfit(prices []int) int {
-	if len(prices) == 0 {
-		return 0
-	}
-
-	sum := 0
-	max := 0
-	temp := 0
-
+	var maxProfit int
 	for i := 1; i < len(prices); i++ {
-
-		if prices[i] < prices[i-1] {
-			sum += max
-			max = 0
-			temp = 0
-		} else {
-			temp += prices[i] - prices[i-1]
-			if max < temp {
-				max = temp
-			}
-		}
-		if i == len(prices)-1 {
-			sum += temp
+		if prices[i-1] < prices[i] {
+			maxProfit += prices[i] - prices[i-1]
 		}
 	}
-	return sum
+	return maxProfit
 }
