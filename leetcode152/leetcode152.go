05d9c530fe1e0c573bafe922309aac98cd5accb7
Julian Chu <yulang.chu@gmail.com>
2019-10-31 23:54:59 +0100

    add leetcode152

--- leetcode152/leetcode152.go
+++ leetcode152/leetcode152.go
@@ -1,20 +1,22 @@
 package leetcode152
 
 func maxProduct(nums []int) int {
-	if len(nums) == 1 {
-		return nums[0]
-	}
-	max := 0
+	pos, neg, max := 1, 1, nums[0]
 	for i := 0; i < len(nums); i++ {
-		temp := 1
-		for j := i; j < len(nums); j++ {
-			temp *= nums[j]
-			if temp > max {
-				max = temp
-			}
-			if temp == 0 {
-				temp = 1
-			}
+		switch {
+		case nums[i] > 0:
+			pos, neg = nums[i]*pos, nums[i]*neg
+		case nums[i] < 0:
+			pos, neg = nums[i]*neg, nums[i]*pos
+		default:
+			pos, neg = 0, 1
+		}
+
+		if max < pos {
+			max = pos
+		}
+		if pos <= 0 {
+			pos = 1
 		}
 	}
 	return max
