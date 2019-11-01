130dc932b6c23151eec5e08ab7cbafe8803de283
Julian Chu <yulang.chu@gmail.com>
2019-11-02 00:58:19 +0100

    add leetcode162

--- leetcode162/leetcode162.go
+++ leetcode162/leetcode162.go
@@ -5,10 +5,10 @@ func findPeakElement(nums []int) int {
 		return 0
 	}
 
-	for i := 0; i < len(nums)-1; i++ {
-		if nums[i] > nums[i+1] {
+	for i := 0; i < len(nums); i++ {
+		if (i == 0 || nums[i] > nums[i-1]) && (i == len(nums)-1 || nums[i] > nums[i+1]) {
 			return i
 		}
 	}
-	return len(nums) - 1
+	return 0
 }
--- leetcode162/leetcode162_test.go
+++ leetcode162/leetcode162_test.go
@@ -9,40 +9,56 @@ func Test_findPeakElement(t *testing.T) {
 	tests := []struct {
 		name string
 		args args
-		want int
+		want []int
 	}{
 		{
 			name: "[1,2,3,1]",
 			args: args{
 				nums: []int{1, 2, 3, 1},
 			},
-			want: 2,
+			want: []int{2},
 		},
 		{
 			name: "[1,2,1,3,5,6,4]",
 			args: args{
 				nums: []int{1, 2, 1, 3, 5, 6, 4},
 			},
-			want: 1,
+			want: []int{1, 5},
 		},
 		{
 			name: "[1,2]",
 			args: args{
 				nums: []int{1, 2},
 			},
-			want: 1,
+			want: []int{1},
 		},
 		{
 			name: "[2,1]",
 			args: args{
 				nums: []int{2, 1},
 			},
-			want: 0,
+			want: []int{0},
 		},
+		{
+			name: "[1,3,2,1]",
+			args: args{
+				nums: []int{1, 3, 2, 1},
+			},
+			want: []int{1},
+		},
+	}
+
+	var contains = func(answers []int, got int) bool {
+		for _, v := range answers {
+			if got == v {
+				return true
+			}
+		}
+		return false
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := findPeakElement(tt.args.nums); got != tt.want {
+			if got := findPeakElement(tt.args.nums); !contains(tt.want, got) {
 				t.Errorf("findPeakElement() = %v, want %v", got, tt.want)
 			}
 		})
