02f0af62c68e5e92670a923b8b3337f686616833
Julian Chu <yulang.chu@gmail.com>
2019-11-07 23:26:11 +0100

    add leetcode91

--- leetcode91/leetcode91.go
+++ leetcode91/leetcode91.go
@@ -1,7 +1,5 @@
 package leetcode91
 
-import "strconv"
-
 func numDecodings(s string) int {
 	cnt := 0
 	rec([]byte(s), &cnt)
@@ -14,7 +12,7 @@ func rec(restStr []byte, cnt *int) {
 		return
 	}
 	// take 1
-	v1, _ := strconv.ParseInt(string(restStr[0]), 10, 32)
+	v1 := restStr[0] - '0'
 	if v1 != 0 {
 		rec(append([]byte{}, restStr[1:]...), cnt)
 	}
@@ -23,7 +21,7 @@ func rec(restStr []byte, cnt *int) {
 		return
 	}
 
-	v2, _ := strconv.ParseInt(string(restStr[:2]), 10, 32)
+	v2 := v1*10 + restStr[1] - '0'
 	if v2 > 26 || v2 == 0 || v1 == 0 {
 		return
 	}
