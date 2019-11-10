c69b44b830c797d53e8a9ea457268f9eb3a26fb8
Julian Chu <yulang.chu@gmail.com>
2019-11-10 12:40:38 +0100

    add leetcode96

--- leetcode95/leetcode95.go
+++ leetcode95/leetcode95.go
@@ -1,5 +1,7 @@
 package leetcode95
 
+import "fmt"
+
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
@@ -10,62 +12,35 @@ func generateTrees(n int) []*TreeNode {
 	if n == 0 {
 		return []*TreeNode{}
 	}
-	in := make([]int, n)
-	for i := 0; i < n; i++ {
-		in[i] = i + 1
-	}
 
-	pres := getPres(in)
-
-	return buildTreeNodes(in, pres)
+	return helper(1, n+1, map[string][]*TreeNode{})
 }
 
-func buildTreeNodes(in []int, pres [][]int) []*TreeNode {
-	nodes := make([]*TreeNode, len(pres))
+func helper(start int, end int, visited map[string][]*TreeNode) []*TreeNode {
+	key := fmt.Sprintf("%d-%d", start, end)
+	if _, ok := visited[key]; ok {
+		return visited[key]
+	}
 
-	for i, pre := range pres {
-		nodes[i] = buildNode(in, pre)
+	if start == end {
+		return []*TreeNode{nil}
 	}
-	return nodes
-}
 
-func buildNode(in []int, pre []int) *TreeNode {
-	if len(pre) == 0 {
-		return nil
-	}
-	node := &TreeNode{Val: pre[0]}
-	nodeInIdx := 0
-	for i := range in {
-		if in[i] == pre[0] {
-			nodeInIdx = i
-			break
-		}
-	}
-	node.Left = buildNode(in[0:nodeInIdx], pre[1:nodeInIdx+1])
-	node.Right = buildNode(in[nodeInIdx+1:], pre[nodeInIdx+1:])
-	return node
-}
-
-func getPres(in []int) [][]int {
-	if len(in) < 2 {
-		return [][]int{in}
-	}
-	if len(in) == 2 {
-		return [][]int{{in[0], in[1]}, {in[1], in[0]}}
-	}
-	res := make([][]int, 0)
-	for i := 0; i < len(in); i++ {
-		root := in[i]
-		left := getPres(in[:i])
-		right := getPres(in[i+1:])
-		for j := 0; j < len(left); j++ {
-			for k := 0; k < len(right); k++ {
-				pre := []int{root}
-				pre = append(pre, left[j]...)
-				pre = append(pre, right[k]...)
-				res = append(res, pre)
+	var result []*TreeNode
+	for i := start; i < end; i++ {
+		left := helper(start, i, visited)
+		right := helper(i+1, end, visited)
+		for l := 0; l < len(left); l++ {
+			for r := 0; r < len(right); r++ {
+				root := &TreeNode{
+					Val:   i,
+					Left:  left[l],
+					Right: right[r],
+				}
+				result = append(result, root)
 			}
 		}
 	}
-	return res
+	visited[key] = result
+	return result
 }
