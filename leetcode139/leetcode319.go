546319f7e851572271c06e0340c0e8e5e0b23372
Julian Chu <yulang.chu@gmail.com>
2019-11-11 22:25:02 +0100

    add leetcode139

--- leetcode139/leetcode319.go
+++ leetcode139/leetcode319.go
@@ -3,30 +3,36 @@ package leetcode139
 import "sort"
 
 func wordBreak(s string, wordDict []string) bool {
-	sort.Slice(wordDict, func(i, j int) bool {
-		return len(wordDict[i]) > len(wordDict[j])
-	})
-	strLen := len(s)
-	var recur func(int) bool
-
-	recur = func(start int) bool {
-		for _, v := range wordDict {
-			if start == strLen {
-				return true
-			}
-			n := len(v)
-			end := start + n
-			if end > strLen {
-				continue
-			}
-			if s[start:end] == v {
-				if recur(end) {
-					return true
-				}
-			}
-		}
+	if len(wordDict) == 0 {
 		return false
 	}
 
-	return recur(0)
+	dict := make(map[string]bool, len(wordDict))
+	length := make(map[int]bool, len(wordDict))
+	for _, w := range wordDict {
+		dict[w] = true
+		length[len(w)] = true
+	}
+
+	sizes := make([]int, 0, len(length))
+	for l := range length {
+		sizes = append(sizes, l)
+	}
+
+	sort.Ints(sizes)
+	dp := make([]bool, len(s)+1)
+	dp[0] = true
+	n := len(s)
+	for i := 0; i <= n; i++ {
+		if !dp[i] {
+			continue
+		}
+
+		for _, size := range sizes {
+			if i+size <= n {
+				dp[i+size] = dp[i+size] || dict[s[i:i+size]]
+			}
+		}
+	}
+	return dp[n]
 }
