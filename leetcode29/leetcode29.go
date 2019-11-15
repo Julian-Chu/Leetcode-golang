99763bd91bb505fac1aee2ca53d35b1539f8147e
Julian Chu <yulang.chu@gmail.com>
2019-11-15 23:38:10 +0100

    add leetcode29

--- leetcode29/leetcode29.go
+++ leetcode29/leetcode29.go
@@ -3,34 +3,36 @@ package leetcode29
 import "math"
 
 func divide(dividend int, divisor int) int {
-	if divisor == 0 || dividend == 0 {
+	if divisor == 0 {
 		return 0
 	}
+	res, _ := d(int(math.Abs(float64(dividend))), int(math.Abs(float64(divisor))), 1)
 
-	res := divInt64(int64(math.Abs(float64(dividend))), int64(math.Abs(float64(divisor))))
-
-	if (dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0) {
+	if (divisor > 0 && dividend < 0) || (divisor < 0 && dividend > 0) {
 		res = -res
 	}
+
 	if res > math.MaxInt32 {
 		return math.MaxInt32
 	}
-	if -res < math.MinInt32 {
+	if res < math.MinInt32 {
 		return math.MinInt32
 	}
-	return int(res)
+	return res
 }
 
-func divInt64(dividend int64, divisor int64) int64 {
-	if dividend < divisor {
-		return 0
-	}
-	cnt := 1
-	temp := divisor
-	for dividend > (temp << 1) {
-		temp = temp << 1
-		cnt = cnt << 1
-	}
+func d(m, n, cnt int) (res, remainder int) {
+	switch {
+	case m < n:
+		return 0, m
+	case m >= n && m < n+n:
+		return cnt, m - n
+	default:
+		res, remainder = d(m, n+n, cnt+cnt)
+		if remainder >= n {
+			return res + cnt, remainder - n
+		}
 
-	return int64(cnt) + divInt64(dividend-temp, divisor)
+		return
+	}
 }
