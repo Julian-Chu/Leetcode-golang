50d95c6294ffe6477aed92c8b0a128fdebd51c1a
yu-lang.chu <yu-lang.chu@redheads.de>
2019-12-02 12:10:12 +0100

    add leetcode94

--- leetcode94/leetcode94.go
+++ leetcode94/leetcode94.go
@@ -7,5 +7,17 @@ type TreeNode struct {
 }
 
 func inorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return []int{}
+	}
+	var l []int
+	var r []int
+	if root.Left != nil {
+		l = inorderTraversal(root.Left)
+	}
+	if root.Right != nil {
+		r = inorderTraversal(root.Right)
+	}
 
+	return append(append(l, root.Val), r...)
 }
---
+++ leetcode94/leetcode94_test.go
@@ -0,0 +1,60 @@
+package leetcode94
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_inorderTraversal(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "case1",
+			args: args{
+				root: &TreeNode{
+					Val: 1,
+					Right: &TreeNode{
+						Val: 2,
+						Left: &TreeNode{
+							Val: 3,
+						},
+					},
+				},
+			},
+			want: []int{1, 3, 2},
+		},
+		{
+			name: "case2",
+			args: args{
+				root: &TreeNode{
+					Val: 1,
+					Left: &TreeNode{
+						Val:   4,
+						Left:  nil,
+						Right: nil,
+					},
+					Right: &TreeNode{
+						Val: 2,
+						Left: &TreeNode{
+							Val: 3,
+						},
+					},
+				},
+			},
+			want: []int{4, 1, 3, 2},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := inorderTraversal(tt.args.root); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("inorderTraversal() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
