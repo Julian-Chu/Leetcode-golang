af8af5bd5385d146dc99be1ab6abd346e7b2c1ea
yu-lang.chu <yu-lang.chu@redheads.de>
2019-12-03 11:15:21 +0100

    add leetcode98

--- leetcode98/leetcode98.go
+++ leetcode98/leetcode98.go
@@ -1,70 +1,34 @@
 package leetcode98
 
-type TreeNode struct {
-	Val   int
-	Left  *TreeNode
-	Right *TreeNode
-}
+import (
+	"Leetcode-golang/utils"
+)
+
+type TreeNode = utils.TreeNode
 
 func isValidBST(root *TreeNode) bool {
-	if root == nil {
-		return true
-	}
-
-	l := true
-	if root.Left != nil {
-		if root.Val <= root.Left.Val {
-			return false
-		}
-		l = isValidSubBST(root.Left, nil, root)
-	}
-	r := true
-	if root.Right != nil {
-		if root.Val >= root.Right.Val {
-			return false
-		}
-		r = isValidSubBST(root.Right, root, nil)
-	}
-
-	return l && r
+	return isValidSubBST(root, nil, nil)
 }
 
 func isValidSubBST(node *TreeNode, minNode *TreeNode, maxNode *TreeNode) bool {
-	l := true
-	if node.Left != nil {
-		if node.Val <= node.Left.Val {
-			return false
-		}
-		if minNode != nil && node.Left.Val <= minNode.Val {
-			return false
-		}
-		if maxNode != nil && node.Left.Val >= maxNode.Val {
-			return false
-		}
-		if minNode != nil {
-			l = isValidSubBST(node.Left, minNode, node)
-		} else {
-			l = isValidSubBST(node.Left, nil, node)
-		}
-	}
-	r := true
-	if node.Right != nil {
-		if node.Val >= node.Right.Val {
-			return false
-		}
-
-		if minNode != nil && node.Right.Val <= minNode.Val {
-			return false
-		}
-		if maxNode != nil && node.Right.Val >= maxNode.Val {
-			return false
-		}
-		if maxNode != nil {
-			r = isValidSubBST(node.Right, node, maxNode)
-		} else {
-			r = isValidSubBST(node.Right, node, nil)
-		}
+	if node == nil {
+		return true
 	}
 
-	return l && r
+	if minNode != nil && node.Val <= minNode.Val {
+		return false
+	}
+
+	if maxNode != nil && node.Val >= maxNode.Val {
+		return false
+	}
+
+	if !isValidSubBST(node.Left, minNode, node) {
+		return false
+	}
+
+	if !isValidSubBST(node.Right, node, maxNode) {
+		return false
+	}
+	return true
 }
