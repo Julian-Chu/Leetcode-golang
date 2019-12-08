3a329fe6b56e337e106d82ea15452776f5f51021
Julian Chu <yulang.chu@gmail.com>
2019-12-08 22:40:24 +0100

    leetcode111

--- leetcode111/leetcode111.go
+++ leetcode111/leetcode111.go
@@ -12,31 +12,19 @@ func minDepth(root *TreeNode) int {
 	if root.Left == nil && root.Right == nil {
 		return 1
 	}
-	return findMinDepth(root, 1)
-}
 
-func findMinDepth(node *TreeNode, cur int) int {
-	if node.Left == nil && node.Right == nil {
-		return cur
-	}
-	l := cur
-	r := cur
-	if node.Left != nil {
-		l = findMinDepth(node.Left, cur+1)
+	l := minDepth(root.Left)
+	r := minDepth(root.Right)
+
+	if root.Left == nil || root.Right == nil {
+		if l > r {
+			return l + 1
+		}
+		return r + 1
 	}
 
-	if node.Right != nil {
-		r = findMinDepth(node.Right, cur+1)
-	}
-
-	if node.Right == nil {
-		return l
-	}
-	if node.Left == nil {
-		return r
-	}
 	if l > r {
-		return r
+		return r + 1
 	}
-	return l
+	return l + 1
 }
