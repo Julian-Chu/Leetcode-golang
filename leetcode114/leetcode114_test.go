388b2808fbee100610d6047060d5c52303c71828
yu-lang.chu <yu-lang.chu@redheads.de>
2019-12-12 14:23:10 +0100

    add leetcode114

---
+++ leetcode114/leetcode114_test.go
@@ -0,0 +1,19 @@
+package leetcode114
+
+import "testing"
+
+func Test_flatten(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+		})
+	}
+}
