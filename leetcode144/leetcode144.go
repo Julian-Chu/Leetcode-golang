0746bf3258f16087bb454f8760ba7b13641ffce4
yu-lang.chu <yu-lang.chu@redheads.de>
2019-12-17 14:44:55 +0100

    add leetcode144

--- leetcode144/leetcode144.go
+++ leetcode144/leetcode144.go
@@ -15,23 +15,27 @@ type TreeNode = utils.TreeNode
 
 func preorderTraversal(root *TreeNode) []int {
 	var res []int
-	return dfs(root, res)
-}
-
-func dfs(node *TreeNode, res []int) []int {
-	if node == nil {
-		return res
-	}
-	res = append(res, node.Val)
-
-	if node.Left == nil && node.Right == nil {
-		return res
-	}
-	if node.Left != nil {
-		res = dfs(node.Left, res)
-	}
-	if node.Right != nil {
-		res = dfs(node.Right, res)
+	cur := root
+	var stack []*TreeNode
+	for cur != nil || len(stack) != 0 {
+		if cur != nil {
+			res = append(res, cur.Val)
+			if cur.Left == nil && cur.Right == nil {
+				cur = nil
+				continue
+			}
+			if cur.Right != nil {
+				stack = append(stack, cur.Right)
+			}
+			if cur.Left != nil {
+				cur = cur.Left
+			} else {
+				cur = nil
+			}
+		} else if len(stack) != 0 {
+			cur = stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+		}
 	}
 	return res
 }
