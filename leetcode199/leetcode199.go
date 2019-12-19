d66bf6e04731232eb2e37c27736a27812b37674b
yu-lang.chu <yu-lang.chu@redheads.de>
2019-12-19 13:56:46 +0100

    add leetcode199

--- leetcode199/leetcode199.go
+++ leetcode199/leetcode199.go
@@ -13,21 +13,23 @@ import "Leetcode-golang/utils"
 type TreeNode = utils.TreeNode
 
 func rightSideView(root *TreeNode) []int {
-	var res []int
-	queue := []*TreeNode{root}
-	for len(queue) > 0 {
-		size := len(queue)
-		res = append(res, queue[size-1].Val)
-		for i := range queue {
-			node := queue[i]
-			if node.Left != nil {
-				queue = append(queue, node.Left)
-			}
-			if node.Right != nil {
-				queue = append(queue, node.Right)
-			}
-		}
-		queue = queue[size:]
+	if root == nil {
+		return nil
 	}
+
+	if root.Left == nil && root.Right == nil {
+		return []int{root.Val}
+	}
+
+	ls := rightSideView(root.Left)
+	rs := rightSideView(root.Right)
+
+	if len(ls) > len(rs) {
+		rs = append(rs, ls[len(rs):]...)
+	}
+
+	res := make([]int, len(rs)+1)
+	res[0] = root.Val
+	copy(res[1:], rs)
 	return res
 }
