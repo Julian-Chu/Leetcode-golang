acd83b8798e2ca57ad7fdfb6eb0a2f1c529c89e9
Julian Chu <yulang.chu@gmail.com>
2020-01-26 19:39:32 +0100

    leetcode86

--- leetcode86/leetcode86.go
+++ leetcode86/leetcode86.go
@@ -5,36 +5,28 @@ import "Leetcode-golang/helper"
 type ListNode = helper.ListNode
 
 func partition(head *ListNode, x int) *ListNode {
-	q := make([]*ListNode, 0)
-
-	cur := head
-	var prev, insertBefore *ListNode
-
-	for cur != nil {
-		if insertBefore == nil && cur.Val >= x {
-			insertBefore = cur
-		}
-		if cur.Val < x {
-			q = append(q, cur)
-			if prev != nil {
-				prev.Next = cur.Next
-			}
-
-		} else {
-			prev = cur
-		}
-		cur = cur.Next
-	}
-
-	if len(q) == 0 {
+	if head == nil || head.Next == nil {
 		return head
 	}
-	newHead := q[0]
 
-	for i := 1; i < len(q); i++ {
-		q[i-1].Next = q[i]
+	lessHead := &ListNode{}
+	moreEqHead := &ListNode{}
+
+	lessEnd := lessHead
+	moreEqEnd := moreEqHead
+
+	for head != nil {
+		if head.Val < x {
+			lessEnd.Next = head
+			lessEnd = lessEnd.Next
+		} else {
+			moreEqEnd.Next = head
+			moreEqEnd = moreEqEnd.Next
+		}
+		head = head.Next
 	}
-	q[len(q)-1].Next = insertBefore
 
-	return newHead
+	lessEnd.Next = moreEqHead.Next
+	moreEqEnd.Next = nil
+	return lessHead.Next
 }
