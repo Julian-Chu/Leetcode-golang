34138244bba3fa269ba7e71b90159f965315fa15
Julian Chu <yulang.chu@gmail.com>
2020-01-30 00:03:32 +0100

    leetcode150

--- leetcode150/leetcode150.go
+++ leetcode150/leetcode150.go
@@ -9,25 +9,22 @@ func evalRPN(tokens []string) int {
 	numStack := make([]int, 0, len(tokens))
 
 	for _, v := range tokens {
-		num, err := strconv.Atoi(v)
-		if err == nil {
+		if num, err := strconv.Atoi(v); err == nil {
 			numStack = append(numStack, num)
 			continue
 		}
 		v1, v2 := numStack[len(numStack)-2], numStack[len(numStack)-1]
 		numStack = numStack[:len(numStack)-2]
-		res := 0
 		switch v {
 		case "+":
-			res = v1 + v2
+			numStack = append(numStack, v1+v2)
 		case "-":
-			res = v1 - v2
+			numStack = append(numStack, v1-v2)
 		case "*":
-			res = v1 * v2
+			numStack = append(numStack, v1*v2)
 		case "/":
-			res = v1 / v2
+			numStack = append(numStack, v1/v2)
 		}
-		numStack = append(numStack, res)
 	}
 
 	return numStack[0]
