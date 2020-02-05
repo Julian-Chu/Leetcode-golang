84f936971e1d3782cb6e2e8056d50004325cee46
yu-lang.chu <yu-lang.chu@redheads.de>
2020-02-05 13:37:45 +0100

    add leetcode21

--- leetcode21/leetcode21.go
+++ leetcode21/leetcode21.go
@@ -19,30 +19,12 @@ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 		return l1
 	}
 
-	beforeHead := &ListNode{}
-	cur := beforeHead
-	for l1 != nil && l2 != nil {
-		if l1.Val <= l2.Val {
-			cur.Next = l1
-			l1 = l1.Next
-		} else {
-			cur.Next = l2
-			l2 = l2.Next
-		}
-		cur = cur.Next
+	if l1.Val <= l2.Val {
+		l1.Next = mergeTwoLists(l1.Next, l2)
+		return l1
+	} else {
+		l2.Next = mergeTwoLists(l1, l2.Next)
+		return l2
 	}
 
-	for l1 != nil {
-		cur.Next = l1
-		l1 = l1.Next
-		cur = cur.Next
-	}
-
-	for l2 != nil {
-		cur.Next = l2
-		l2 = l2.Next
-		cur = cur.Next
-	}
-
-	return beforeHead.Next
 }
