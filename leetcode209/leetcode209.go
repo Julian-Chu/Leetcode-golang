10163330f04212988f1c409916d761638c7b68f8
yu-lang.chu <yu-lang.chu@redheads.de>
2020-02-10 20:45:57 +0100

    add leetcode209

--- leetcode209/leetcode209.go
+++ leetcode209/leetcode209.go
@@ -1,31 +1,22 @@
 package leetcode209
 
 func minSubArrayLen(s int, nums []int) int {
-	length := len(nums)
-	if length == 0 {
-		return 0
-	}
-	res := length + 1
-	for l, r, sum := 0, 0, nums[0]; l < length && r < length; {
-		if sum < s {
-			r++
-			if r >= length {
-				break
+	n := len(nums)
+
+	res, i, j, sum := n+1, 0, 0, 0
+
+	for j < n {
+		sum += nums[j]
+		j++
+
+		for sum >= s {
+			sum -= nums[i]
+			i++
+			if res > j-i+1 {
+				res = j - i + 1
 			}
-			sum += nums[r]
-			continue
 		}
-
-		if r-l+1 < res {
-			res = r - l + 1
-		}
-
-		sum -= nums[l]
-		l++
-	}
-	if res > length {
-		return 0
 	}
 
-	return res
+	return res % (n + 1)
 }
