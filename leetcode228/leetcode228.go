774138ad0570e4316d5d686930817e206147550b
Julian Chu <yulang.chu@gmail.com>
2020-02-12 22:49:08 +0100

    add leetcode228(better)

--- leetcode228/leetcode228.go
+++ leetcode228/leetcode228.go
@@ -1,35 +1,61 @@
 package leetcode228
 
-import (
-	"fmt"
-	"strconv"
-)
+import "fmt"
 
 func summaryRanges(nums []int) []string {
-	if len(nums) == 0 {
-		return []string{}
+	res := []string{}
+
+	l := len(nums)
+	if l == 0 {
+		return res
 	}
 
-	if len(nums) == 1 {
-		return []string{strconv.Itoa(nums[0])}
-	}
+	begin := nums[0]
+	str := ""
 
-	res := make([]string, 0)
-	start, end := nums[0], nums[0]
-	nums = append(nums, nums[len(nums)-1])
-	for i := 1; i < len(nums); i++ {
-		if nums[i]-nums[i-1] == 1 {
-			end = nums[i]
-			continue
+	for i := 0; i < l; i++ {
+		if i == l-1 || nums[i]+1 != nums[i+1] {
+			if nums[i] == begin {
+				str = fmt.Sprintf("%d", begin)
+			} else {
+				str = fmt.Sprintf("%d->%d", begin, nums[i])
+			}
+
+			if i+1 < l {
+				begin = nums[i+1]
+			}
+
+			res = append(res, str)
 		}
-
-		if start == end {
-			res = append(res, strconv.Itoa(start))
-		} else {
-			res = append(res, fmt.Sprintf("%v->%v", start, end))
-		}
-		start, end = nums[i], nums[i]
 	}
-
 	return res
 }
+
+//func summaryRanges(nums []int) []string {
+//	if len(nums) == 0 {
+//		return []string{}
+//	}
+//
+//	if len(nums) == 1 {
+//		return []string{strconv.Itoa(nums[0])}
+//	}
+//
+//	res := make([]string, 0)
+//	start, end := nums[0], nums[0]
+//	nums = append(nums, nums[len(nums)-1])
+//	for i := 1; i < len(nums); i++ {
+//		if nums[i]-nums[i-1] == 1 {
+//			end = nums[i]
+//			continue
+//		}
+//
+//		if start == end {
+//			res = append(res, strconv.Itoa(start))
+//		} else {
+//			res = append(res, fmt.Sprintf("%v->%v", start, end))
+//		}
+//		start, end = nums[i], nums[i]
+//	}
+//
+//	return res
+//}
