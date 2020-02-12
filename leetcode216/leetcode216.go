61be53d0d0257daf61d8f1a9fdc5267a5c5a714c
Julian Chu <yulang.chu@gmail.com>
2020-02-12 22:19:22 +0100

    leetcode216(dfs)

--- leetcode216/leetcode216.go
+++ leetcode216/leetcode216.go
@@ -1,65 +1,33 @@
 package leetcode216
 
 func combinationSum3(k int, n int) [][]int {
-	if k == 0 || n < 3 {
-		return [][]int{}
-	}
+	res := [][]int{}
+	temp := make([]int, k+1)
+	used := make([]bool, 10)
 
-	q := make([]map[int]bool, 0, 9)
-	res := make([][]int, 0)
-	for i := 0; i < 9; i++ {
-		q = append(q, map[int]bool{i + 1: true})
-	}
+	var dfs func(int, int)
 
-	for len(q) > 0 {
-		if len(q[0]) == k {
-			break
-		}
-		next := make([]map[int]bool, 0)
-
-		for idx := range q {
-			sum := 0
-			max := 0
-			for v := range q[idx] {
-				sum += v
-				if v > max {
-					max = v
-				}
+	dfs = func(idx int, remain int) {
+		if idx == k {
+			if remain < 10 && !used[remain] {
+				temp[idx] = remain
+				t := make([]int, k)
+				copy(t, temp[1:])
+				res = append(res, t)
 			}
-			upper := 9
-			if n-sum < 9 {
-				upper = n - sum
-			}
-			for i := 0; i < upper; i++ {
-				if i+1 <= max {
-					continue
-				}
-				if _, ok := q[idx][i+1]; ok {
-					continue
-				}
-				newMap := make(map[int]bool)
-				for k, v := range q[idx] {
-					newMap[k] = v
-				}
-				newMap[i+1] = true
-				next = append(next, newMap)
-			}
+			return
 		}
-		q = next
-	}
 
-	for i := range q {
-		s := make([]int, 0, k)
-		sum := 0
-		for v := range q[i] {
-			s = append(s, v)
-			sum += v
+		for i := temp[idx-1] + 1; i < 10; i++ {
+			if remain-i < i {
+				return
+			}
+			used[i] = true
+			temp[idx] = i
+			dfs(idx+1, remain-i)
+			used[i] = false
 		}
-		if sum != n {
-			continue
-		}
-		res = append(res, s)
 	}
-
+	dfs(1, n)
 	return res
 }
