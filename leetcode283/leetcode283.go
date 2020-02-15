de6e501079ecb1289095305cfcbe6f31027e8bbf
Julian Chu <yulang.chu@gmail.com>
2020-02-15 10:42:32 +0100

    add leetcode283

--- leetcode283/leetcode283.go
+++ leetcode283/leetcode283.go
@@ -1,26 +1,16 @@
 package leetcode283
 
 func moveZeroes(nums []int) {
-	l := len(nums)
-	if l == 0 || l == 1 {
-		return
-	}
+	replace := 0
 
-	for i := l - 2; i >= 0; i-- {
+	for i := 0; i < len(nums); i++ {
 		if nums[i] != 0 {
-			continue
-		}
-
-		if nums[i+1] == 0 {
-			continue
-		}
-
-		for j := i; j < l-1; j++ {
-			for nums[j+1] != 0 {
-				nums[j], nums[j+1] = nums[j+1], nums[j]
-			}
+			nums[replace] = nums[i]
+			replace++
 		}
 	}
 
-	return
+	for i := replace; i < len(nums); i++ {
+		nums[i] = 0
+	}
 }
