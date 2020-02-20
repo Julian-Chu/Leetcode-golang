471da06e3acebaf88e09a1524ead83bad9d3650d
yu-lang.chu <yu-lang.chu@redheads.de>
2020-02-20 11:05:17 +0100

    add leetcode279(timeout)

---
+++ leetcode279/leetcode279.go
@@ -0,0 +1,35 @@
+package leetcode279
+
+func numSquares(n int) int {
+	sn := make([]int, 0)
+
+	for i := 1; i < n; i++ {
+		v := i * i
+
+		if v > n {
+			break
+		}
+		sn = append(sn, v)
+	}
+	minCnt := n
+	var recur func(int, []int)
+	recur = func(rest int, curRes []int) {
+		if rest == 0 {
+			if len(curRes) < minCnt {
+				minCnt = len(curRes)
+			}
+			return
+		}
+		curRes = curRes[:len(curRes):len(curRes)]
+		for i := len(sn) - 1; i >= 0; i-- {
+			if sn[i] > rest {
+				continue
+			}
+
+			recur(rest-sn[i], append(curRes, sn[i]))
+		}
+	}
+
+	recur(n, []int{})
+	return minCnt
+}
