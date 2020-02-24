cbf2cb148f0572509b4c51e96c82a1512d46cb75
Julian Chu <yulang.chu@gmail.com>
2020-02-24 23:12:15 +0100

    add leetcode309

--- leetcode309/leetcode309.go
+++ leetcode309/leetcode309.go
@@ -2,17 +2,24 @@ package leetcode309
 
 func maxProfit(prices []int) int {
 	l := len(prices)
+	if l == 0 {
+		return 0
+	}
+	s0 := 0          //rest after rest, rest after sell
+	s1 := -prices[0] // buy after rest, rest after buy
+	s2 := -1 << 31   //  sell after rest
 
-	buy := make([]int, l+1)
-	sell := make([]int, l+1)
+	for i := 0; i < len(prices); i++ {
+		pre0 := s0
+		pre1 := s1
+		pre2 := s2
 
-	buy[1] = -prices[0]
-	for i := 2; i <= l; i++ {
-		buy[i] = max(buy[i-1], sell[i-2]-prices[i-1])
-		sell[i] = max(sell[i-1], prices[i-1]+buy[i-1])
+		s0 = max(pre0, pre2)
+		s1 = max(pre0-prices[i], pre1)
+		s2 = s1 + prices[i]
 	}
 
-	return sell[l]
+	return max(s0, s2)
 }
 
 func max(a, b int) int {
