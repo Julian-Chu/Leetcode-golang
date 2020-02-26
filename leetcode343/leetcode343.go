9d47d245b235eeaf82536e3aca1ad610bf252477
yu-lang.chu <yu-lang.chu@redheads.de>
2020-02-26 14:23:03 +0100

    add leetcode343

--- leetcode343/leetcode343.go
+++ leetcode343/leetcode343.go
@@ -1,28 +1,20 @@
 package leetcode343
 
 func integerBreak(n int) int {
-	switch n {
-	case 2:
+	if n == 2 {
 		return 1
-	case 3:
+	}
+
+	if n == 3 {
 		return 2
 	}
 
-	dp := make([]int, n+1)
+	res := 1
 
-	dp[2] = 2
-	dp[3] = 3
-
-	for i := 4; i < n+1; i++ {
-		max := 0
-		for j := 1; j < i; j++ {
-			res := j * dp[i-j]
-			if res > max {
-				max = res
-			}
-		}
-		dp[i] = max
+	for n > 4 {
+		res *= 3
+		n -= 3
 	}
 
-	return dp[n]
+	return res * n
 }
