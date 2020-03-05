c9c3d70d7a67b838b40895c97d94be57cd3f6936
yu-lang.chu <yu-lang.chu@redheads.de>
2020-03-05 14:40:58 +0100

    add leetcode416

--- leetcode416/leetcode416.go
+++ leetcode416/leetcode416.go
@@ -16,34 +16,12 @@ func canPartition(nums []int) bool {
 	eqVal := sum >> 1
 
 	dp := make([]bool, eqVal+1)
+
 	dp[0] = true
-	dp_slice := make([][]int, eqVal+1)
 
-	for i := range dp {
-		if i == 0 {
-			dp_slice[i] = append([]int{}, nums...)
-			continue
-		}
-
-	loop:
-		for j := i - 1; j >= 0; j-- {
-			v := i - j
-			for idx := range dp_slice[j] {
-				if dp[j] == false {
-					continue
-				}
-				if dp_slice[j][idx] == v {
-					lastIdx := len(dp_slice[j]) - 1
-					dp_slice[j][lastIdx], dp_slice[j][idx] = dp_slice[j][idx], dp_slice[j][lastIdx]
-					dp_slice[i] = make([]int, lastIdx)
-					copy(dp_slice[i], dp_slice[j][:lastIdx])
-					dp[i] = true
-					break loop
-				}
-			}
-		}
-		if dp[i] == false {
-			dp_slice[i] = append([]int{}, nums...)
+	for _, num := range nums {
+		for i := eqVal; i >= num; i-- {
+			dp[i] = dp[i] || dp[i-num]
 		}
 	}
 
