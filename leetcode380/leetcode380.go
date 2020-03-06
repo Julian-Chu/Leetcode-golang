864a58a17288509d1af42673b759586943b222ac
yu-lang.chu <yu-lang.chu@redheads.de>
2020-03-06 12:26:57 +0100

    add leetcode380

--- leetcode380/leetcode380.go
+++ leetcode380/leetcode380.go
@@ -3,38 +3,34 @@ package leetcode380
 import "math/rand"
 
 type RandomizedSet struct {
-	numMapper map[int]bool
-	nums      []int
+	idx  map[int]int
+	nums []int
 }
 
 /** Initialize your data structure here. */
 func Constructor() RandomizedSet {
-	return RandomizedSet{numMapper: make(map[int]bool), nums: make([]int, 0)}
+	return RandomizedSet{idx: make(map[int]int), nums: make([]int, 0)}
 }
 
 /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
 func (this *RandomizedSet) Insert(val int) bool {
-	if _, ok := this.numMapper[val]; !ok {
-		this.numMapper[val] = true
-		this.nums = append(this.nums, val)
-		return true
+	if _, ok := this.idx[val]; ok {
+		return false
 	}
-	return false
+	this.nums = append(this.nums, val)
+	this.idx[val] = len(this.nums) - 1
+	return true
 }
 
 /** Removes a value from the set. Returns true if the set contained the specified element. */
 func (this *RandomizedSet) Remove(val int) bool {
-	if _, ok := this.numMapper[val]; ok {
-		delete(this.numMapper, val)
-		for i := range this.nums {
-			if this.nums[i] == val {
-				size := len(this.nums)
-				this.nums[i], this.nums[size-1] = this.nums[size-1], this.nums[i]
-				this.nums = this.nums[: size-1 : size-1]
-				break
-			}
-		}
-
+	if i, ok := this.idx[val]; ok {
+		size := len(this.nums)
+		this.nums[i], this.nums[size-1] = this.nums[size-1], this.nums[i]
+		this.idx[this.nums[i]] = i
+		this.nums = this.nums[:size-1]
+		// move delete to last due to case that remove last element
+		delete(this.idx, val)
 		return true
 	}
 	return false
