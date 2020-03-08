603d280b504e4f0a575ded9a0e972ccb9fa9ad0a
Julian Chu <yulang.chu@gmail.com>
2020-03-08 17:04:14 +0100

    add leetcode258 and 204

---
+++ leetcdoe258/leetcode258.go
@@ -0,0 +1,5 @@
+package leetcdoe258
+
+func addDigits(num int) int {
+	return num % 9
+}
--- leetcode204/leetcode204.go
+++ leetcode204/leetcode204.go
@@ -1,33 +1,24 @@
 package leetcode204
 
 func countPrimes(n int) int {
-	if n <= 1 {
+	if n < 3 {
 		return 0
 	}
-	dp := make([]bool, n)
+	isComposite := make([]bool, n)
 
-	for i := range dp {
-		dp[i] = true
-	}
-	dp[0] = false
-	dp[1] = false
+	cnt := n / 2
 
-	for i := 2; i < n; i++ {
-		if dp[i] == false {
+	for i := 3; i*i < n; i += 2 {
+		if isComposite[i] {
 			continue
 		}
 
-		for j := i * i; j < n; {
-			dp[j] = false
-			j += i
-		}
-	}
-
-	cnt := 0
-
-	for _, v := range dp {
-		if v {
-			cnt++
+		for j := i * i; j < n; j += 2 * i {
+			if isComposite[j] {
+				continue
+			}
+			cnt--
+			isComposite[j] = true
 		}
 	}
 
