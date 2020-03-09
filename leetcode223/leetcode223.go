7711e160d82c74889478716e880413143c9fd6c9
yu-lang.chu <yu-lang.chu@redheads.de>
2020-03-09 15:14:01 +0100

    add leetcode223

--- leetcode223/leetcode223.go
+++ leetcode223/leetcode223.go
@@ -1,47 +1,25 @@
 package leetcode223
 
 func computeArea(A int, B int, C int, D int, E int, F int, G int, H int) int {
-	type Rec struct {
-		top, bottom, left, right int
-	}
 
-	getArea := func(r Rec) int {
-		area := (r.top - r.bottom) * (r.left - r.right)
+	getArea := func(A, B, C, D int) int {
+		area := (A - C) * (B - D)
 
 		if area < 0 {
 			area = -area
 		}
 		return area
 	}
-	first := Rec{
-		top:    D,
-		bottom: B,
-		left:   A,
-		right:  C,
-	}
-	areaFirst := getArea(first)
-	second := Rec{
-		top:    H,
-		bottom: F,
-		left:   E,
-		right:  G,
-	}
-	areaSecond := getArea(second)
+	areaFirst := getArea(A, B, C, D)
+	areaSecond := getArea(E, F, G, H)
 	// check no overlap
 	areaOverlap := 1
-	if first.top < second.bottom || first.bottom > second.top || first.right < second.left || first.left > second.right {
+	if A > G || C < E || D < F || B > H {
 		areaOverlap = 0
 	}
 
 	if areaOverlap != 0 {
-		overlap := Rec{
-			top:    min(first.top, second.top),
-			bottom: max(first.bottom, second.bottom),
-			left:   max(first.left, second.left),
-			right:  min(first.right, second.right),
-		}
-
-		areaOverlap = getArea(overlap)
+		areaOverlap = getArea(max(A, E), max(B, F), min(C, G), min(D, H))
 	}
 
 	return areaFirst - areaOverlap + areaSecond
