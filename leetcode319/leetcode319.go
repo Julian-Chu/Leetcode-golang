5100ca1c07e458fec07cbd23aab856a79418bdd1
yu-lang.chu <yu-lang.chu@redheads.de>
2020-03-12 18:05:17 +0100

    add leetcode319(square root)

--- leetcode319/leetcode319.go
+++ leetcode319/leetcode319.go
@@ -1,19 +1,7 @@
 package leetcode319
 
+import "math"
+
 func bulbSwitch(n int) int {
-	res := make([]bool, n+1)
-
-	for i := 1; i < n+1; i++ {
-		for j := i; j < n+1; j += i {
-			res[j] = !res[j]
-		}
-	}
-
-	cnt := 0
-	for i := range res {
-		if res[i] == true {
-			cnt++
-		}
-	}
-	return cnt
+	return int(math.Sqrt(float64(n)))
 }
