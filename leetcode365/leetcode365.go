e0660f045ec921cd3904bb6aa7af02fcce7dd181
Julian Chu <yulang.chu@gmail.com>
2020-03-13 23:48:29 +0100

    add leetcode365

---
+++ leetcode365/leetcode365.go
@@ -0,0 +1,25 @@
+package leetcode365
+
+func canMeasureWater(x int, y int, z int) bool {
+	if z == 0 {
+		return true
+	}
+
+	if x+y < z {
+		return false
+	}
+
+	if x > y {
+		x, y = y, x
+	}
+
+	if x == 0 {
+		return y == z
+	}
+
+	for y%x != 0 {
+		x, y = y%x, x
+	}
+
+	return z%x == 0
+}
