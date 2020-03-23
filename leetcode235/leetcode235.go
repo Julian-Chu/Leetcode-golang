1ad51247b810fd105c8156a689a557e9aedfac4b
yu-lang.chu <yu-lang.chu@redheads.de>
2020-03-23 18:47:54 +0100

    add leetcode235

--- leetcode235/leetcode235.go
+++ leetcode235/leetcode235.go
@@ -5,10 +5,6 @@ import "Leetcode-golang/utils"
 type TreeNode = utils.TreeNode
 
 func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
-	if p.Val > q.Val {
-		p, q = q, p
-	}
-
 	switch {
 	case root.Val < p.Val && root.Val < q.Val:
 		return lowestCommonAncestor(root.Right, p, q)
