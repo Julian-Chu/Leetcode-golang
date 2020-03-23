46c61b19b35f17f7bd3ce0e6b231d411a0333c54
yu-lang.chu <yu-lang.chu@redheads.de>
2020-03-23 20:12:14 +0100

    add leetcode257

---
+++ leetcode257/leetcode257_test.go
@@ -0,0 +1,51 @@
+package leetcode257
+
+import (
+	"Leetcode-golang/utils"
+	"testing"
+)
+
+func Test_binaryTreePaths(t *testing.T) {
+	type args struct {
+		node []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "[1,2,3,null,5]",
+			args: args{
+				node: []int{1, 2, 3, utils.NULL, 5},
+			},
+			want: []string{"1->2->5", "1->3"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			compareStringSlice := func(s1, s2 []string) bool {
+				if len(s1) != len(s2) {
+					return false
+				}
+
+				for i := range s1 {
+					flag := false
+					for j := range s2 {
+						if s1[i] == s2[j] {
+							flag = true
+							break
+						}
+					}
+					if !flag {
+						return false
+					}
+				}
+				return true
+			}
+			if got := binaryTreePaths(utils.Ints2Tree(tt.args.node)); !compareStringSlice(got, tt.want) {
+				t.Errorf("binaryTreePaths() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
