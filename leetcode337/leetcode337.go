f4823599ccdf72f9ae5f5c204bdc7502d6692848
yu-lang.chu <yu-lang.chu@redheads.de>
2020-03-24 23:15:06 +0100

    add leetcode337

--- leetcode337/leetcode337.go
+++ leetcode337/leetcode337.go
@@ -17,46 +17,24 @@ func rob(root *TreeNode) int {
 		return 0
 	}
 
-	a := includeRoot(root)
-	b := notIncludeRoot(root)
+	var dfs func(*TreeNode) (int, int)
+	dfs = func(node *TreeNode) (withRoot int, woRoot int) {
+		if node == nil {
+			return 0, 0
+		}
 
+		lwr, lwor := dfs(node.Left)
+		rwr, rwor := dfs(node.Right)
+
+		return node.Val + lwor + rwor, max(lwr, lwor) + max(rwr, rwor)
+	}
+
+	return max(dfs(root))
+}
+
+func max(a, b int) int {
 	if a > b {
 		return a
 	}
-
 	return b
-}
-
-func notIncludeRoot(root *TreeNode) int {
-	if root == nil {
-		return 0
-	}
-
-	var l1, l2, r1, r2 int
-	l1 = includeRoot(root.Left)
-	l2 = notIncludeRoot(root.Left)
-
-	r1 = includeRoot(root.Right)
-	r2 = notIncludeRoot(root.Right)
-
-	l := l1
-	if l2 > l1 {
-		l = l2
-	}
-	r := r1
-	if r2 > r1 {
-		r = r2
-	}
-	return l + r
-}
-
-func includeRoot(root *TreeNode) int {
-	if root == nil {
-		return 0
-	}
-	var l1, r1 int
-	l1 = notIncludeRoot(root.Left)
-	r1 = notIncludeRoot(root.Right)
-
-	return l1 + r1 + root.Val
 }
