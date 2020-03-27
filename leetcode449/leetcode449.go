16cf027f52d1647def4606c1d022e30725a1f426
Julian Chu <yulang.chu@gmail.com>
2020-03-27 22:52:29 +0100

    add leetcode449

--- leetcode449/leetcode449.go
+++ leetcode449/leetcode449.go
@@ -51,31 +51,25 @@ func (this *Codec) deserialize(data string) *TreeNode {
 	}
 	nodes := strings.Split(data, ",")
 
-	nums := make([]int, len(nodes))
-
-	for i := range nodes {
-		v, _ := strconv.Atoi(nodes[i])
-		nums[i] = v
-	}
-
-	return this.buildTree(nums)
+	return this.buildTree(nodes)
 }
 
-func (this *Codec) buildTree(nodes []int) *TreeNode {
+func (this *Codec) buildTree(nodes []string) *TreeNode {
 	leftIdx := 0
 	rightIdx := 0
 
 	rootVal := nodes[0]
 	for i := 1; i < len(nodes); i++ {
-		if leftIdx == 0 && nodes[i] < rootVal {
+		if leftIdx == 0 && this.compareValInString(nodes[i], rootVal) < 0 {
 			leftIdx = i
 		}
-		if rightIdx == 0 && nodes[i] > rootVal {
+		if rightIdx == 0 && this.compareValInString(nodes[i], rootVal) > 0 {
 			rightIdx = i
 		}
 	}
 
-	root := &TreeNode{Val: rootVal}
+	v, _ := strconv.Atoi(rootVal)
+	root := &TreeNode{Val: v}
 	if rightIdx != 0 {
 		root.Right = this.buildTree(nodes[rightIdx:])
 	}
@@ -87,6 +81,25 @@ func (this *Codec) buildTree(nodes []int) *TreeNode {
 	}
 
 	return root
+}
+
+func (this Codec) compareValInString(str1, str2 string) int {
+	switch {
+	case len(str1) > len(str2):
+		return 1
+	case len(str1) < len(str2):
+		return -1
+	}
+
+	for i := range str1 {
+		switch {
+		case str1[i] > str2[i]:
+			return 1
+		case str1[i] < str2[i]:
+			return -1
+		}
+	}
+	return 0
 }
 
 /**
