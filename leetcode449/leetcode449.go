731fcba5e4ae0f1342beb259072cc9f124689a63
Julian Chu <yulang.chu@gmail.com>
2020-03-27 22:32:44 +0100

    add leetcode449

--- leetcode449/leetcode449.go
+++ leetcode449/leetcode449.go
@@ -22,52 +22,21 @@ func Constructor() Codec {
 	return Codec{}
 }
 
-const NULL string = "NULL"
-
 // Serializes a tree to a single string.
 func (this *Codec) serialize(root *TreeNode) string {
-	res := ""
 	if root == nil {
-		return res
-	}
-	q := []*TreeNode{root}
-	for len(q) > 0 {
-		q_next := make([]*TreeNode, 0)
-		for i := range q {
-			if q[i] != nil {
-				res += strconv.Itoa(q[i].Val)
-				res += ","
-				if q[i].Left != nil {
-					q_next = append(q_next, q[i].Left)
-				} else {
-					q_next = append(q_next, nil)
-				}
-
-				if q[i].Right != nil {
-					q_next = append(q_next, q[i].Right)
-				} else {
-					q_next = append(q_next, nil)
-				}
-			} else {
-				res += NULL
-				res += ","
-				q_next = append(q_next, nil)
-				q_next = append(q_next, nil)
-			}
-		}
-		q = nil
-		for i := range q_next {
-			if q_next[i] != nil {
-				q = q_next
-				break
-			}
-		}
+		return ""
 	}
 
-	if len(res) > 0 {
-		return res[:len(res)-1]
+	res := strconv.Itoa(root.Val)
+	res += ","
+	if root.Left != nil {
+		res += this.serialize(root.Left)
 	}
 
+	if root.Right != nil {
+		res += this.serialize(root.Right)
+	}
 	return res
 }
 
@@ -76,49 +45,37 @@ func (this *Codec) deserialize(data string) *TreeNode {
 	if data == "" {
 		return nil
 	}
-	nodes := strings.Split(data, ",")
 
-	l := len(nodes)
-	batchsize := 1
-	var root *TreeNode
-	start := 0
-	var q []*TreeNode
-	for batchsize <= l {
-		var q_next []*TreeNode
-		ns := nodes[start : start+batchsize]
-		if len(q) == 0 {
-			num, _ := strconv.Atoi(ns[0])
-			root = &TreeNode{Val: num}
-			q_next = append(q_next, root)
-		} else {
-			for i := range q {
-				if q[i] == nil {
-					q_next = append(q_next, nil)
-					q_next = append(q_next, nil)
-					continue
-				}
-				if ns[i*2] != NULL {
-					num, _ := strconv.Atoi(ns[i*2])
-					node := &TreeNode{Val: num}
-					q[i].Left = node
-					q_next = append(q_next, node)
-				} else {
-					q_next = append(q_next, nil)
-				}
-				if ns[i*2+1] != NULL {
-					num, _ := strconv.Atoi(ns[i*2+1])
-					node := &TreeNode{Val: num}
-					q[i].Right = node
-					q_next = append(q_next, node)
-				} else {
-					q_next = append(q_next, nil)
-				}
-			}
-		}
-		start += batchsize
-		batchsize *= 2
-		q = q_next
+	if data[len(data)-1] == ',' {
+		data = data[:len(data)-1]
 	}
+	nodes := strings.Split(data, ",")
+	leftIdx := 0
+	rightIdx := 0
+
+	rootVal, _ := strconv.Atoi(nodes[0])
+	for i := 1; i < len(nodes); i++ {
+		num, _ := strconv.Atoi(nodes[i])
+		if leftIdx == 0 && num < rootVal {
+			leftIdx = i
+		}
+		if rightIdx == 0 && num > rootVal {
+			rightIdx = i
+		}
+	}
+
+	num, _ := strconv.Atoi(nodes[0])
+	root := &TreeNode{Val: num}
+	if rightIdx != 0 {
+		root.Right = this.deserialize(strings.Join(nodes[rightIdx:], ","))
+	}
+	if leftIdx != 0 {
+		if rightIdx == 0 {
+			rightIdx = len(nodes)
+		}
+		root.Left = this.deserialize(strings.Join(nodes[leftIdx:rightIdx], ","))
+	}
+
 	return root
 }
 
--- leetcode449/leetcode449_test.go
+++ leetcode449/leetcode449_test.go
@@ -6,9 +6,55 @@ import (
 	"testing"
 )
 
+func TestCodec_serializeAndDeserialize(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *TreeNode
+	}{
+		{
+			name: "case2",
+			args: args{
+				root: utils.Ints2Tree([]int{
+					41, 37, 44, 24, 39, 42, 48, 1, 35, 38, 40, utils.NULL, 43, 46, 49, 0, 2, 30, 36, utils.NULL, utils.NULL, utils.NULL, utils.NULL, utils.NULL, utils.NULL, 45, 47, utils.NULL, utils.NULL, utils.NULL, utils.NULL, utils.NULL, 4, 29, 32, utils.NULL, utils.NULL, utils.NULL, utils.NULL, utils.NULL, utils.NULL, 3, 9, 26, utils.NULL, 31, 34, utils.NULL, utils.NULL, 7, 11, 25, 27, utils.NULL, utils.NULL, 33, utils.NULL, 6, 8, 10, 16, utils.NULL, utils.NULL, utils.NULL, 28, utils.NULL, utils.NULL, 5, utils.NULL, utils.NULL, utils.NULL, utils.NULL, utils.NULL, 15, 19, utils.NULL, utils.NULL, utils.NULL, utils.NULL, 12, utils.NULL, 18, 20, utils.NULL, 13, 17, utils.NULL, utils.NULL, 22, utils.NULL, 14, utils.NULL, utils.NULL, 21, 23,
+				}),
+			},
+			want: utils.Ints2Tree([]int{
+				41, 37, 44, 24, 39, 42, 48, 1, 35, 38, 40, utils.NULL, 43, 46, 49, 0, 2, 30, 36, utils.NULL, utils.NULL, utils.NULL, utils.NULL, utils.NULL, utils.NULL, 45, 47, utils.NULL, utils.NULL, utils.NULL, utils.NULL, utils.NULL, 4, 29, 32, utils.NULL, utils.NULL, utils.NULL, utils.NULL, utils.NULL, utils.NULL, 3, 9, 26, utils.NULL, 31, 34, utils.NULL, utils.NULL, 7, 11, 25, 27, utils.NULL, utils.NULL, 33, utils.NULL, 6, 8, 10, 16, utils.NULL, utils.NULL, utils.NULL, 28, utils.NULL, utils.NULL, 5, utils.NULL, utils.NULL, utils.NULL, utils.NULL, utils.NULL, 15, 19, utils.NULL, utils.NULL, utils.NULL, utils.NULL, 12, utils.NULL, 18, 20, utils.NULL, 13, 17, utils.NULL, utils.NULL, 22, utils.NULL, 14, utils.NULL, utils.NULL, 21, 23,
+			}),
+		},
+		{
+			name: "41 37 44 24 39 42 48 1",
+			args: args{
+				root: &TreeNode{
+					Val:   41,
+					Left:  &utils.TreeNode{Val: 37, Left: &utils.TreeNode{Val: 24, Left: &utils.TreeNode{Val: 1}}, Right: &utils.TreeNode{Val: 39}},
+					Right: &utils.TreeNode{Val: 44, Left: &utils.TreeNode{Val: 42}, Right: &TreeNode{Val: 48}},
+				},
+			},
+			want: &TreeNode{
+				Val:   41,
+				Left:  &utils.TreeNode{Val: 37, Left: &utils.TreeNode{Val: 24, Left: &utils.TreeNode{Val: 1}}, Right: &utils.TreeNode{Val: 39}},
+				Right: &utils.TreeNode{Val: 44, Left: &utils.TreeNode{Val: 42}, Right: &TreeNode{Val: 48}},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			this := &Codec{}
+			str := this.serialize(tt.args.root)
+			if got := this.deserialize(str); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("deserialize() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
 func TestCodec_serialize(t *testing.T) {
 	type args struct {
-		root []int
+		root *TreeNode
 	}
 	tests := []struct {
 		name string
@@ -16,24 +62,53 @@ func TestCodec_serialize(t *testing.T) {
 		want string
 	}{
 		{
-			name: "[1,2]",
+			name: "41 37 44 24 39 42",
 			args: args{
-				root: []int{1, 2, utils.NULL},
+				root: &TreeNode{
+					Val:   41,
+					Left:  &utils.TreeNode{Val: 37, Left: &utils.TreeNode{Val: 24}, Right: &utils.TreeNode{Val: 39}},
+					Right: &utils.TreeNode{Val: 44, Left: &utils.TreeNode{Val: 42}},
+				},
 			},
-			want: "1,2,NULL",
+			want: "41,37,24,39,44,42,",
 		},
 		{
-			name: "[1,2,NULL,NULL,3,NULL,NULL]",
+			name: "[1,2]",
 			args: args{
-				root: []int{1, 2, utils.NULL, utils.NULL, 3, utils.NULL, utils.NULL},
+				root: &TreeNode{
+					Val:   1,
+					Right: &utils.TreeNode{Val: 2},
+				},
 			},
-			want: "1,2,NULL,NULL,3,NULL,NULL",
+			want: "1,2,",
+		},
+		{
+			name: "[1,2,3]",
+			args: args{
+				root: &TreeNode{
+					Val: 1,
+					Right: &utils.TreeNode{Val: 2,
+						Right: &utils.TreeNode{Val: 3}},
+				},
+			},
+			want: "1,2,3,",
+		},
+		{
+			name: "[5,2,6]",
+			args: args{
+				root: &TreeNode{
+					Val:   5,
+					Left:  &utils.TreeNode{Val: 2},
+					Right: &utils.TreeNode{Val: 6},
+				},
+			},
+			want: "5,2,6,",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			this := &Codec{}
-			if got := this.serialize(utils.Ints2Tree(tt.args.root)); got != tt.want {
+			if got := this.serialize(tt.args.root); got != tt.want {
 				t.Errorf("serialize() = %v, want %v", got, tt.want)
 			}
 		})
@@ -47,23 +122,47 @@ func TestCodec_deserialize(t *testing.T) {
 	tests := []struct {
 		name string
 		args args
-		want []int
+		want *TreeNode
 	}{
-		// TODO: Add test cases.
 		{
-			name: "[1,2,NULL,NULL,3,NULL,NULL]",
+			name: "41 37 44 24 39 42 48 1",
 			args: args{
-				data: "1,2,NULL,NULL,3,NULL,NULL",
+				data: "41,37,24,1,39,44,42,48,",
 			},
-			want: []int{1, 2, utils.NULL, utils.NULL, 3, utils.NULL, utils.NULL},
+			want: &TreeNode{
+				Val:   41,
+				Left:  &utils.TreeNode{Val: 37, Left: &utils.TreeNode{Val: 24, Left: &utils.TreeNode{Val: 1}}, Right: &utils.TreeNode{Val: 39}},
+				Right: &utils.TreeNode{Val: 44, Left: &utils.TreeNode{Val: 42}, Right: &TreeNode{Val: 48}},
+			},
+		},
+		{
+			name: "[1,2,3]",
+			args: args{
+				data: "1,2,3,",
+			},
+			want: &TreeNode{
+				Val: 1,
+				Right: &utils.TreeNode{Val: 2,
+					Right: &TreeNode{Val: 3}},
+			},
+		},
+		{
+			name: "[5,2,6]",
+			args: args{
+				data: "5,2,6,",
+			},
+			want: &TreeNode{
+				Val:   5,
+				Left:  &utils.TreeNode{Val: 2},
+				Right: &utils.TreeNode{Val: 6},
+			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			this := &Codec{}
-			want := utils.Ints2Tree(tt.want)
-			if got := this.deserialize(tt.args.data); !reflect.DeepEqual(got, want) {
-				t.Errorf("deserialize() = %v, want %v", tt.name, tt.want)
+			if got := this.deserialize(tt.args.data); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("deserialize() = %v, want %v", got, tt.want)
 			}
 		})
 	}
