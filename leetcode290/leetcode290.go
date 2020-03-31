9ebdb02652596d78df456be15b9ec5cda6c5b5e9
yu-lang.chu <yu-lang.chu@redheads.de>
2020-03-31 11:04:36 +0200

    add leetcode290

--- leetcode290/leetcode290.go
+++ leetcode290/leetcode290.go
@@ -3,32 +3,33 @@ package leetcode290
 import "strings"
 
 func wordPattern(pattern string, str string) bool {
-	if len(pattern) == 0 {
-		return false
-	}
-
-	words := strings.Split(str, " ")
+	words := strings.Fields(str)
 	if len(pattern) != len(words) {
 		return false
 	}
 
-	m := make(map[byte]string)
-	wordSet := make(map[string]bool)
-	for i := range pattern {
-		v, ok := m[pattern[i]]
-		if !ok {
-			if _, exist := wordSet[words[i]]; exist {
-				return false
-			}
-			m[pattern[i]] = words[i]
-			wordSet[words[i]] = true
-			continue
+	m1 := make(map[byte]string)
+	m2 := make(map[string]byte)
+
+	for i := 0; i < len(pattern); i++ {
+		b := pattern[i]
+		s := words[i]
+
+		bs, ok1 := m1[b]
+		sb, ok2 := m2[s]
+
+		if !ok1 && !ok2 {
+			m1[b] = s
+			m2[s] = b
 		}
 
-		if v != words[i] {
+		if ok1 && ok2 && (b != sb || s != bs) {
+			return false
+		}
+
+		if (ok1 && !ok2) || (!ok1 && ok2) {
 			return false
 		}
 	}
 	return true
-
 }
