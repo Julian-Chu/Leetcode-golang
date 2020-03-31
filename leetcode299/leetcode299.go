db6d138be9bd95234302eec527b25fbdb25d358f
yu-lang.chu <yu-lang.chu@redheads.de>
2020-03-31 12:14:49 +0200

    add leetcode299

--- leetcode299/leetcode299.go
+++ leetcode299/leetcode299.go
@@ -3,35 +3,24 @@ package leetcode299
 import "fmt"
 
 func getHint(secret string, guess string) string {
-	bs1 := []byte(secret)
-	bs2 := []byte(guess)
-	m := make(map[byte]int)
-	cntA := 0
-	for i := range bs1 {
-		m[bs1[i]]++
+	bull, cow := 0, 0
+
+	count := make(map[rune]int)
+
+	for _, ch := range secret {
+		count[ch]++
 	}
 
-	for i, v := range bs2 {
-		cnt, ok := m[v]
-		if !ok {
-			continue
+	for k, v := range guess {
+		if count[v] > 0 {
+			cow++
+			count[v]--
 		}
 
-		if bs1[i] == v {
-			cntA++
-		}
-
-		if cnt > 0 {
-			m[v]--
+		if secret[k] == byte(v) {
+			bull++
+			cow--
 		}
 	}
-
-	cntB := len(bs1) - cntA
-
-	for _, v := range m {
-		cntB -= v
-	}
-
-	return fmt.Sprintf("%vA%vB", cntA, cntB)
-
+	return fmt.Sprintf("%dA%dB", bull, cow)
 }
