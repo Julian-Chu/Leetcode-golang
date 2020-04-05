ec4805abc2d9cf2ab9563eaccdcae5834378a00c
Julian Chu <yulang.chu@gmail.com>
2020-04-05 13:12:13 +0200

    add leetcode438

--- leetcode438/leetcode438.go
+++ leetcode438/leetcode438.go
@@ -1,58 +1,33 @@
 package leetcode438
 
 func findAnagrams(s string, p string) []int {
-	if len(s) == 0 {
+	n := len(p)
+	if len(s) == 0 || len(s) < n {
 		return []int{}
 	}
 
-	res := make([]int, 0)
-	m := make(map[byte]int)
+	target := [26]int{}
 
-	n := len(p)
-	var xorRes byte = 0
-	sumRes := 0
-	for i, _ := range p {
-		xorRes ^= p[i]
-		sumRes += int(p[i])
-		m[p[i]]++
+	for i := range p {
+		target[p[i]-'a']++
 	}
 
-	xor := uint8(0)
-	sum := 0
+	window := [26]int{}
+	res := make([]int, 0)
 	for i := 0; i <= len(s)-n; i++ {
 		if i == 0 {
-			for j := 0; j < n; j++ {
-				xor ^= s[j]
-				sum += int(s[j])
-			}
-		} else {
-			xor ^= s[i-1]
-			xor ^= s[i+n-1]
-			sum -= int(s[i-1])
-			sum += int(s[i+n-1])
-		}
-
-		if xor == xorRes && sum == sumRes {
-			msub := make(map[byte]int)
 			subStr := s[i : i+n]
 			for j := range subStr {
-				msub[subStr[j]]++
+				window[subStr[j]-'a']++
 			}
-
-			flag := true
-			for key, v := range msub {
-				cnt, ok := m[key]
-				if !ok || (ok && cnt != v) {
-					flag = false
-					break
-				}
-			}
-			if flag {
-				res = append(res, i)
-			}
+		} else {
+			window[s[i-1]-'a']--
+			window[s[i+n-1]-'a']++
 		}
-
+		if target == window {
+			res = append(res, i)
+		}
 	}
-
 	return res
+
 }
