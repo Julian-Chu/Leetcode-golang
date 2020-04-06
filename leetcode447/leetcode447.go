e37f6442a1ee5c7c2c4c4922edbb6ca33c716309
yu-lang.chu <yu-lang.chu@redheads.de>
2020-04-06 09:25:52 +0200

    add leetcode447

---
+++ leetcode447/leetcode447.go
@@ -0,0 +1,35 @@
+package leetcode447
+
+func numberOfBoomerangs(points [][]int) int {
+	res := 0
+	n := len(points)
+	if n == 0 {
+		return res
+	}
+
+	calcDistSquare := func(p1, p2 []int) int {
+		x := p1[0] - p2[0]
+		y := p1[1] - p2[1]
+
+		return x*x + y*y
+	}
+
+	for i := 0; i < n; i++ {
+		distMap := make(map[int]int)
+		for j := 0; j < n; j++ {
+			if j == i {
+				continue
+			}
+			dist := calcDistSquare(points[i], points[j])
+			distMap[dist]++
+		}
+
+		for _, cnt := range distMap {
+			if cnt > 1 {
+				res += cnt * (cnt - 1)
+			}
+		}
+	}
+
+	return res
+}
---
+++ leetcode447/leetcode447_test.go
@@ -0,0 +1,29 @@
+package leetcode447
+
+import "testing"
+
+func Test_numberOfBoomerangs(t *testing.T) {
+	type args struct {
+		points [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "[[0,0],[1,0],[2,0]]",
+			args: args{
+				points: [][]int{{0, 0}, {1, 0}, {2, 0}},
+			},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := numberOfBoomerangs(tt.args.points); got != tt.want {
+				t.Errorf("numberOfBoomerangs() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
