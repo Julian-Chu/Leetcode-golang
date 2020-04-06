7108757f4d6490bedff825d3286b78041a96c68d
yu-lang.chu <yu-lang.chu@redheads.de>
2020-04-06 10:55:58 +0200

    add leetcode463

--- leetcode463/leetcode463.go
+++ leetcode463/leetcode463.go
@@ -1,37 +1,28 @@
 package leetcode463
 
 func islandPerimeter(grid [][]int) int {
-	l := len(grid)
-	if l == 0 {
-		return 0
-	}
-	w := len(grid[0])
-	if w == 0 {
-		return 0
-	}
-	perimeter := 0
-	for i := 0; i < l; i++ {
-		for j := 0; j < w; j++ {
+	var dx = []int{-1, 1, 0, 0}
+	var dy = []int{0, 0, -1, 1}
+	m, n := len(grid), len(grid[0])
+
+	res := 0
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
 			if grid[i][j] == 0 {
 				continue
 			}
-			if i == 0 || grid[i-1][j] == 0 {
-				perimeter++
-			}
+			res += 4
 
-			if i == l-1 || grid[i+1][j] == 0 {
-				perimeter++
-			}
-
-			if j == 0 || grid[i][j-1] == 0 {
-				perimeter++
-			}
-
-			if j == w-1 || grid[i][j+1] == 0 {
-				perimeter++
+			for k := 0; k < 4; k++ {
+				x := i + dx[k]
+				y := j + dy[k]
+				if x >= 0 && x < m && y >= 0 && y < n && grid[x][y] == 1 {
+					res--
+				}
 			}
 		}
 	}
 
-	return perimeter
+	return res
 }
