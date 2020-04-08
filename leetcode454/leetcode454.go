1a5bccd83c8771032ec8ec01afbedf28cb20c8b6
Julian Chu <yulang.chu@gmail.com>
2020-04-08 20:36:52 +0200

    add leetcode454

--- leetcode454/leetcode454.go
+++ leetcode454/leetcode454.go
@@ -5,28 +5,20 @@ func fourSumCount(A []int, B []int, C []int, D []int) int {
 	if n == 0 {
 		return 0
 	}
-	m1 := make(map[int]int, n*n)
+	sum := make(map[int]int, n*n)
 
-	for i := 0; i < n; i++ {
-		for j := 0; j < n; j++ {
-			m1[A[i]+B[j]]++
-		}
-	}
-	m2 := make(map[int]int, n*n)
-
-	for i := 0; i < n; i++ {
-		for j := 0; j < n; j++ {
-			m2[C[i]+D[j]]++
+	for i := range A {
+		for j := range B {
+			sum[A[i]+B[j]]++
 		}
 	}
 
 	res := 0
-	for sum1, cnt1 := range m1 {
-		sum2 := 0 - sum1
-		if cnt2, ok := m2[sum2]; ok {
-			res += cnt1 * cnt2
+
+	for i := range C {
+		for j := range D {
+			res += sum[-(C[i] + D[j])]
 		}
 	}
-
 	return res
 }
