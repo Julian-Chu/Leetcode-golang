83dc98c5c3318811760c37975992aea5bf99f410
Julian Chu <yulang.chu@gmail.com>
2020-04-12 17:06:48 +0200

    add leetcode332

--- leetcode332/leetcode332.go
+++ leetcode332/leetcode332.go
@@ -3,33 +3,31 @@ package leetcode332
 import "sort"
 
 func findItinerary(tickets [][]string) []string {
-	nexts := make(map[string][]string, len(tickets))
-	res := make([]string, 0, len(tickets))
-
-	for _, ticket := range tickets {
-		nexts[ticket[0]] = append(nexts[ticket[0]], ticket[1])
-	}
-
-	for _, next := range nexts {
-		sort.Strings(next)
-	}
-	var dfs func(from string)
-	dfs = func(from string) {
-		for len(nexts[from]) > 0 {
-			next := nexts[from][0]
-			nexts[from] = nexts[from][1:]
-			dfs(next)
+	tickets_map := make(map[string][]string)
+	for _, arr := range tickets {
+		if _, ok := tickets_map[arr[0]]; ok {
+			tickets_map[arr[0]] = append(tickets_map[arr[0]], arr[1])
+		} else {
+			tickets_map[arr[0]] = []string{arr[1]}
 		}
-		res = append(res, from)
 	}
 
-	dfs("JFK")
-
-	l, r := 0, len(res)-1
-	for l < r {
-		res[l], res[r] = res[r], res[l]
-		l++
-		r--
+	for _, arr := range tickets_map {
+		sort.Strings(arr)
 	}
-	return res
+
+	var ret []string
+	stack := []string{"JFK"}
+	for len(stack) != 0 {
+		key := stack[len(stack)-1]
+		if len(tickets_map[key]) != 0 {
+			var nextkey string
+			nextkey, tickets_map[key] = tickets_map[key][0], tickets_map[key][1:]
+			stack = append(stack, nextkey)
+		} else {
+			ret = append([]string{key}, ret...)
+			stack = stack[0 : len(stack)-1]
+		}
+	}
+	return ret
 }
