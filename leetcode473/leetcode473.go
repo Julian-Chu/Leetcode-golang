141cd236c5811bd6b3d817ae1daec3b4ab962323
Julian Chu <yulang.chu@gmail.com>
2020-04-16 23:48:45 +0200

    add leetcode473

--- leetcode473/leetcode473.go
+++ leetcode473/leetcode473.go
@@ -6,46 +6,41 @@ func makesquare(nums []int) bool {
 	if len(nums) < 4 {
 		return false
 	}
-	sort.Sort(sort.Reverse(sort.IntSlice(nums)))
 
 	sum := 0
 	for _, num := range nums {
 		sum += num
 	}
 
-	squareLen := sum / 4
-	if sum%4 != 0 || nums[len(nums)-1] > squareLen || nums[0] > squareLen {
+	avg := sum / 4
+	if sum%4 != 0 || nums[len(nums)-1] > avg || nums[0] > avg {
 		return false
 	}
 
-	visited := make([]bool, len(nums))
+	sort.Sort(sort.Reverse(sort.IntSlice(nums)))
+
+	edges := make([]int, 4)
 	var dfs func(int, int) bool
-	dfs = func(index int, rest int) bool {
-		if rest == 0 {
-			return true
-		}
-		if rest < 0 {
+	dfs = func(index int, target int) bool {
+		if index == len(nums) {
+			if edges[0] == target && edges[1] == target && edges[2] == target {
+				return true
+			}
 			return false
 		}
 
-		for index < len(nums) {
-			if !visited[index] {
-				visited[index] = true
-				if dfs(index, rest-nums[index]) {
-					return true
-				}
-				visited[index] = false
+		for i := 0; i < 4; i++ {
+			if edges[i]+nums[index] > target {
+				continue
 			}
-			index++
+			edges[i] += nums[index]
+			if dfs(index+1, target) {
+				return true
+			}
+			edges[i] -= nums[index]
 		}
 		return false
 	}
 
-	res := dfs(0, squareLen) && dfs(0, squareLen) && dfs(0, squareLen) && dfs(0, squareLen)
-	for _, v := range visited {
-		if v == false {
-			return false
-		}
-	}
-	return res
+	return dfs(0, avg)
 }
