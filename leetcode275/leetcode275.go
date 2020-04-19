1e0b64907590d4d0ec8e9fdf89b1049f65707f71
Julian Chu <yulang.chu@gmail.com>
2020-04-19 16:44:21 +0200

    add leetcode275

--- leetcode275/leetcode275.go
+++ leetcode275/leetcode275.go
@@ -2,21 +2,22 @@ package leetcode275
 
 func hIndex(citations []int) int {
 	n := len(citations)
-
-	l, r := 0, n-1
-	for l < r {
-		citations[l], citations[r] = citations[r], citations[l]
-		l++
-		r--
-	}
-	for i := 1; i <= n; i++ {
-		if citations[i-1] >= i {
-			continue
+	hIdx := 0
+	for idx := 0; idx <= n; idx++ {
+		gt := 0
+		eq := 0
+		for i := range citations {
+			switch {
+			case citations[i] > idx:
+				gt++
+			case citations[i] == idx:
+				eq++
+			}
 		}
-
-		if citations[i-1] < i {
-			return i - 1
+		if gt+eq >= idx {
+			hIdx = idx
 		}
 	}
-	return n
+
+	return hIdx
 }
