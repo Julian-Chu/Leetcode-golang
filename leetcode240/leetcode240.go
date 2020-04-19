83390999a467538d6f9d44641a908d399dba2718
Julian Chu <yulang.chu@gmail.com>
2020-04-19 10:54:58 +0200

    add leetcode240

--- leetcode240/leetcode240.go
+++ leetcode240/leetcode240.go
@@ -7,50 +7,18 @@ func searchMatrix(matrix [][]int, target int) bool {
 	m := len(matrix)
 	n := len(matrix[0])
 
-	k := m
-	if m > n {
-		k = n
-	}
-	start := k - 1
-	for i := 0; i < k; i++ {
-		if matrix[i][i] == target {
+	i, j := 0, n-1
+
+	for i < m && j >= 0 {
+		val := matrix[i][j]
+
+		switch {
+		case val > target:
+			j--
+		case val < target:
+			i++
+		default:
 			return true
-		}
-		if target < matrix[i][i] {
-			if i == 0 {
-				return false
-			}
-			start = i - 1
-			break
-		}
-	}
-
-	for idx := 0; idx < start+1; idx++ {
-		for i := idx; i < n; i++ {
-			if matrix[idx][i] == target {
-				return true
-			}
-		}
-
-		if idx+1 < m {
-			for i := 0; i < idx; i++ {
-				if matrix[idx+1][i] == target {
-					return true
-				}
-			}
-		}
-		for j := idx; j < m; j++ {
-			if matrix[j][idx] == target {
-				return true
-			}
-		}
-
-		if idx+1 < n {
-			for j := 0; j < idx; j++ {
-				if matrix[j][idx+1] == target {
-					return true
-				}
-			}
 		}
 	}
 
