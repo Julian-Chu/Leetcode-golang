8b4ffc4ba9006b4eee3d0cd655a8f550f94a41a3
yu-lang.chu <yu-lang.chu@redheads.de>
2020-04-20 11:35:41 +0200

    add leetcode441(quadratic formula)

--- leetcode441/leetcode441.go
+++ leetcode441/leetcode441.go
@@ -1,27 +1,7 @@
 package leetcode441
 
+import "math"
+
 func arrangeCoins(n int) int {
-	calcSumFromZero := func(num int) int {
-		return (num + 1) * num / 2
-	}
-
-	l, r := 0, n
-
-	for l <= r {
-		mid := l + (r-l)/2
-		sum := calcSumFromZero(mid)
-
-		if sum > n {
-			r = mid - 1
-		} else if sum < n {
-			l = mid + 1
-		} else {
-			return mid
-		}
-
-		//if l+1 == r {
-		//	break
-		//}
-	}
-	return r
+	return int(math.Sqrt(float64(1+8*n))-1) / 2
 }
