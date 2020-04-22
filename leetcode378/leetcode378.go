0941194124ca48cc298efcfeb0c61983062319a3
yu-lang.chu <yu-lang.chu@redheads.de>
2020-04-22 12:30:42 +0200

    add leetcode378

--- leetcode378/leetcode378.go
+++ leetcode378/leetcode378.go
@@ -1,58 +1,30 @@
 package leetcode378
 
 func kthSmallest(matrix [][]int, k int) int {
-	if len(matrix) == 0 || len(matrix[0]) == 0 {
-		return 0
-	}
-
 	n := len(matrix)
+	l, r := matrix[0][0], matrix[n-1][n-1]
 
-	res := make([]int, 0, n*n)
-	for i := range matrix {
-		res = mergeSort(res, matrix[i])
-	}
+	for l < r {
+		mid := l + (r-l)>>1
+		cnt := 0
 
-	return res[k-1]
-}
-
-func mergeSort(nums1 []int, nums2 []int) []int {
-	if len(nums1) == 0 {
-		return nums2
-	}
-
-	if len(nums2) == 0 {
-		return nums1
-	}
-
-	i, j := 0, 0
-
-	var res []int
-	for i < len(nums1) || j < len(nums2) {
-		if i < len(nums1) && j < len(nums2) {
-			if nums1[i] > nums2[j] {
-				res = append(res, nums2[j])
-				j++
-			} else if nums1[i] < nums2[j] {
-				res = append(res, nums1[i])
-				i++
-			} else {
-				res = append(res, nums1[i])
-				res = append(res, nums2[j])
-				i++
-				j++
+		for i := range matrix {
+			if matrix[i][0] > mid {
+				break
 			}
-			continue
+			for j := range matrix[i] {
+				if matrix[i][j] > mid {
+					break
+				}
+				cnt++
+			}
 		}
 
-		for i < len(nums1) {
-			res = append(res, nums1[i])
-			i++
-		}
-
-		for j < len(nums2) {
-			res = append(res, nums2[j])
-			j++
+		if cnt >= k {
+			r = mid
+		} else {
+			l = mid + 1
 		}
 	}
-	return res
+	return l
 }
