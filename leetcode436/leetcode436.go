7fc4f73ef88f137c85842a180bc7272bd771e381
yu-lang.chu <yu-lang.chu@redheads.de>
2020-04-22 12:37:24 +0200

    add leetcode436

--- leetcode436/leetcode436.go
+++ leetcode436/leetcode436.go
@@ -3,51 +3,33 @@ package leetcode436
 import "sort"
 
 func findRightInterval(intervals [][]int) []int {
-	if len(intervals) == 0 {
+	n := len(intervals)
+	if n == 0 {
 		return []int{}
 	}
-	if len(intervals) == 1 {
+	if n == 1 {
 		return []int{-1}
 	}
 
-	m := make(map[int]int, len(intervals))
+	m := make(map[int]int, n)
 
+	starts := make([]int, n)
 	for i := range intervals {
 		m[intervals[i][0]] = i
+		starts[i] = intervals[i][0]
 	}
 
-	starts := make([]int, 0, len(intervals))
-	for key, _ := range m {
-		starts = append(starts, key)
-	}
 	sort.Ints(starts)
 
-	findRight := func(interval []int) (index int) {
-		target := interval[1]
-		if target > starts[len(starts)-1] {
-			return -1
+	res := make([]int, n)
+
+	for i, v := range intervals {
+		idx := sort.SearchInts(starts, v[1])
+		if idx < n {
+			res[i] = m[starts[idx]]
+		} else {
+			res[i] = -1
 		}
-
-		l, r := 0, len(starts)-1
-
-		for l < r {
-			mid := l + (r-l)>>1
-			if starts[mid] > target {
-				r = mid
-			} else if starts[mid] < target {
-				l = mid + 1
-			} else {
-				l = mid
-				r = mid
-			}
-		}
-		return m[starts[r]]
-	}
-
-	res := make([]int, len(intervals))
-
-	for i := range intervals {
-		res[i] = findRight(intervals[i])
 	}
 
 	return res
