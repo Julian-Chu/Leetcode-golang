6619897f76d39b5072febd5892041597285ec56b
Julian Chu <yulang.chu@gmail.com>
2020-04-24 20:43:04 +0200

    add leetcode406

--- leetcode406/leetcode406.go
+++ leetcode406/leetcode406.go
@@ -4,61 +4,21 @@ import "sort"
 
 func reconstructQueue(people [][]int) [][]int {
 	sort.Slice(people, func(i, j int) bool {
-		if people[i][1] < people[j][1] {
+		if people[i][0] > people[j][0] {
 			return true
 		}
-
-		if people[i][1] == people[j][1] && people[i][0] < people[j][0] {
+		if people[i][0] == people[j][0] && people[i][1] < people[j][1] {
 			return true
 		}
-
 		return false
 	})
 
-	temp := make([][]int, 0, len(people))
-	res := make([][]int, 0, len(people))
-	for len(people) > 0 || len(temp) > 0 {
-		if len(res) == 0 {
-			res = append(res, people[0])
-			people = people[1:]
-			continue
-		}
-		var p []int
-		if len(people) > 0 {
-			p = people[0]
-			people = people[1:]
-		} else {
-			p = temp[0]
-			temp = temp[1:]
-		}
-		v := p[0]
-		k := p[1]
+	res := make([][]int, len(people))
 
-		i := 0
-
-		for ; i < len(res); i++ {
-			if k == 0 {
-				if res[i][0] < v {
-					continue
-				}
-
-				if res[i][0] >= v {
-					break
-				}
-			}
-			if res[i][0] >= v {
-				k--
-				continue
-			}
-		}
-
-		if k > 0 {
-			temp = append(temp, p)
-		} else {
-			res = append(res, p)
-			copy(res[i+1:], res[i:])
-			res[i] = p
-		}
+	for _, p := range people {
+		idx := p[1]
+		copy(res[idx+1:], res[idx:])
+		res[idx] = p
 	}
 
 	return res
