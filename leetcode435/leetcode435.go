861ad9c04a110ace9d30a896a7f2ab7d1335d224
Julian Chu <yulang.chu@gmail.com>
2020-04-26 11:11:14 +0200

    add leetcode435

--- leetcode435/leetcode435.go
+++ leetcode435/leetcode435.go
@@ -3,21 +3,22 @@ package leetcode435
 import "sort"
 
 func eraseOverlapIntervals(intervals [][]int) int {
-	if len(intervals) == 0 {
+	if len(intervals) <= 1 {
 		return 0
 	}
 	sort.Slice(intervals, func(i, j int) bool {
 		return intervals[i][1] < intervals[j][1]
 	})
 
-	res := make([][]int, 0, len(intervals))
+	res := 0
 	end := -1 << 31
 	for i := range intervals {
 		if intervals[i][0] >= end {
-			res = append(res, intervals[i])
 			end = intervals[i][1]
+		} else {
+			res++
 		}
 	}
 
-	return len(intervals) - len(res)
+	return res
 }
