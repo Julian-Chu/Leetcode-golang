56909e8107a65a9102a05c4719dd90ffc07f7a77
Julian Chu <yulang.chu@gmail.com>
2020-05-27 22:13:40 +0200

    add leetcode424

--- leetcode424/leetcode424.go
+++ leetcode424/leetcode424.go
@@ -1,20 +1,27 @@
 package leetcode424
 
 func characterReplacement(s string, k int) int {
-	var max, l int //max : max frequency of uppercase English letter
-	count := [128]int{}
 
-	for r := 0; r < len(s); r++ {
-		count[s[r]]++
-		max = Max(max, count[s[r]])
-
-		if r-l+1-max > k {
-			count[s[l]]--
-			l++
+	maxLen := 0
+	for ch := byte('A'); ch <= byte('Z'); ch++ {
+		l := 0
+		kLeft := k
+		for r := 0; r < len(s); r++ {
+			if s[r] != ch {
+				if kLeft > 0 {
+					kLeft--
+				} else {
+					maxLen = Max(maxLen, r-l)
+					for s[l] == ch {
+						l++
+					}
+					l++
+				}
+			}
 		}
+		maxLen = Max(maxLen, len(s)-l)
 	}
-
-	return len(s) - l
+	return maxLen
 }
 
 func Max(a int, b int) int {
