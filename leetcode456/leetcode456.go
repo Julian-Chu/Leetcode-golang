43e51c01febd10cbf2f711a33b98bf26b0ffd9ad
Julian Chu <yulang.chu@gmail.com>
2020-06-14 13:30:48 +0200

    add leetcode456

--- leetcode456/leetcode456.go
+++ leetcode456/leetcode456.go
@@ -1,30 +1,22 @@
 package leetcode456
 
 func find132pattern(nums []int) bool {
-	if len(nums) < 3 {
-		return false
+
+	numk := -1 << 31
+
+	stack := make([]int, 0, len(nums))
+
+	for i := len(nums) - 1; i >= 0; i-- {
+		if nums[i] < numk {
+			return true
+		}
+
+		for len(stack) > 0 && stack[len(stack)-1] < nums[i] {
+			numk = stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+		}
+
+		stack = append(stack, nums[i])
 	}
-
-	for i := 0; i < len(nums)-2; i++ {
-		if nums[i+1] < nums[i] {
-			continue
-		}
-		max, min := -1<<31, 1<<31-1
-		if nums[i] < min {
-			min = nums[i]
-		}
-
-		for j := i + 1; j < len(nums); j++ {
-			if nums[j] > max {
-				max = nums[j]
-				continue
-			}
-
-			if nums[j] > min && nums[j] < max {
-				return true
-			}
-		}
-	}
-
 	return false
 }
