381431d45144e49e7747f0c446e7bdadf0f19dba
yu-lang.chu <yu-lang.chu@redheads.de>
2020-06-16 13:07:31 +0200

    add leetcode503

--- leetcode503/leetcode503.go
+++ leetcode503/leetcode503.go
@@ -3,18 +3,21 @@ package leetcode503
 func nextGreaterElements(nums []int) []int {
 	n := len(nums)
 	res := make([]int, n)
+	stack := make([]int, 0, n)
 	for i := 0; i < n; i++ {
 		res[i] = -1
-
-		for j := i + 1; ; j++ {
-			j %= n
-			if j == i {
-				break
-			}
-			if nums[j] > nums[i] {
-				res[i] = nums[j]
-				break
-			}
+		for len(stack) > 0 && nums[stack[len(stack)-1]] < nums[i] {
+			idx := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			res[idx] = nums[i]
+		}
+		stack = append(stack, i)
+	}
+	for i := 0; i < n; i++ {
+		for len(stack) > 0 && nums[stack[len(stack)-1]] < nums[i] {
+			idx := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			res[idx] = nums[i]
 		}
 	}
 
