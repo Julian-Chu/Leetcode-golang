be06e2878368ccab5204bf1456b54f34b129e70f
yu-lang.chu <yu-lang.chu@redheads.de>
2020-06-17 10:12:24 +0200

    add leetcode503

--- leetcode503/leetcode503.go
+++ leetcode503/leetcode503.go
@@ -1,19 +1,44 @@
 package leetcode503
 
 func nextGreaterElements(nums []int) []int {
-	n := len(nums)
-	res := make([]int, n)
-	for i := range res {
-		res[i] = -1
-	}
-	stack := make([]int, 0, n)
+	var stack Stack
 
-	for i := 0; i < n*2; i++ {
-		for len(stack) > 0 && nums[stack[len(stack)-1]] < nums[i%n] {
-			res[stack[len(stack)-1]] = nums[i%n]
-			stack = stack[:len(stack)-1]
+	for i := len(nums) - 2; i >= 0; i-- {
+		for len(stack) > 0 && stack.top() <= nums[i] {
+			stack.pop()
 		}
-		stack = append(stack, i%n)
+		stack.push(nums[i])
 	}
-	return res
+
+	greater := make([]int, len(nums))
+
+	for i := len(nums) - 1; i >= 0; i-- {
+		for len(stack) > 0 && stack.top() <= nums[i] {
+			stack.pop()
+		}
+
+		if len(stack) == 0 {
+			greater[i] = -1
+		} else {
+			greater[i] = stack.top()
+		}
+		stack.push(nums[i])
+	}
+	return greater
+}
+
+type Stack []int
+
+func (s *Stack) push(val int) {
+	*s = append(*s, val)
+}
+
+func (s *Stack) pop() int {
+	val := (*s)[len(*s)-1]
+	*s = (*s)[:len(*s)-1]
+	return val
+}
+
+func (s *Stack) top() int {
+	return (*s)[len(*s)-1]
 }
