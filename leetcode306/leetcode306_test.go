35ce75d0bf8fdab209868c9d99267a1a9a4815d6
Julian Chu <yulang.chu@gmail.com>
2020-06-28 23:56:12 +0200

    add leetcode306

---
+++ leetcode306/leetcode306_test.go
@@ -0,0 +1,43 @@
+package leetcode306
+
+import "testing"
+
+func Test_isAdditiveNumber(t *testing.T) {
+	type args struct {
+		num string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "112",
+			args: args{
+				num: "112",
+			},
+			want: true,
+		},
+		{
+			name: "112358",
+			args: args{
+				num: "112358",
+			},
+			want: true,
+		},
+		{
+			name: "199100199",
+			args: args{
+				num: "199100199",
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isAdditiveNumber(tt.args.num); got != tt.want {
+				t.Errorf("isAdditiveNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
