8765f21c1f8d10ed204d8a4980da67d85c99da53
yu-lang.chu <yu-lang.chu@redheads.de>
2020-07-08 17:26:07 +0200

    add leetcode201

--- leetcode201/leetcode201.go
+++ leetcode201/leetcode201.go
@@ -1,27 +1,16 @@
 package leetcode201
 
 func rangeBitwiseAnd(m int, n int) int {
-	v := 0
-	var i uint
-	for i = 31; i >= 0; i-- {
-		digit := 0
-		cur := m >> i % 2
-		if cur != 0 {
-			digit = 1
-			for j := m; j <= n; j++ {
-				if j>>i%2 == 0 {
-					digit = 0
-					break
-				}
-			}
+	if m == 0 {
+		return 0
+	}
+	var step uint = 0
 
-		}
-		v *= 2
-		v += digit
-		if i == 0 {
-			break
-		}
+	for m != n {
+		m >>= 1
+		n >>= 1
+		step++
 	}
 
-	return v
+	return m << step
 }
