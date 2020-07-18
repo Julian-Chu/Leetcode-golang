9991068a5d3461f17d2a89152b3d55f124c44bac
Julian Chu <yulang.chu@gmail.com>
2020-07-18 12:55:05 +0200

    add leetcode342

--- leetcode342/leetcode342.go
+++ leetcode342/leetcode342.go
@@ -1,18 +1,5 @@
 package leetcode342
 
 func isPowerOfFour(num int) bool {
-	if num == 0 {
-		return false
-	}
-	i := num & 715827882
-	if i != 0 {
-		return false
-	}
-	com := ^num + 1
-	j := num & com
-	if j != num {
-		return false
-	}
-
-	return true
+	return num > 0 && num&(num-1) == 0 && (num&0x55555555) != 0
 }
