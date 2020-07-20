226987dc7bb067345ea0b3ba4f73bfbe2f10e9b0
yu-lang.chu <yu-lang.chu@redheads.de>
2020-07-20 12:53:07 +0200

    add leetcode393

--- leetcode393/leetcode393.go
+++ leetcode393/leetcode393.go
@@ -1,38 +1,25 @@
 package leetcode393
 
 func validUtf8(data []int) bool {
-	byteCnt := 0
-	maskBytes1 := 128 //10000000
-	maskBytes2 := 64  //01000000
-	for _, byte := range data {
-		if byteCnt == 0 {
-			byteCnt = calcByteCnt(byte)
-			if byteCnt == -1 {
+	cnt := 0
+	for _, d := range data {
+		if cnt == 0 {
+			switch {
+			case d>>3 == 30:
+				cnt = 3
+			case d>>4 == 14:
+				cnt = 2
+			case d>>5 == 6:
+				cnt = 1
+			case d>>7 > 0:
 				return false
 			}
-			byteCnt--
-			continue
-		}
-
-		byteCnt--
-		if byte&maskBytes1 != maskBytes1 || byte&maskBytes2 == maskBytes2 {
-			return false
+		} else {
+			if d>>6 != 2 {
+				return false
+			}
+			cnt--
 		}
 	}
-	return byteCnt == 0
-}
-
-func calcByteCnt(b int) int {
-	switch {
-	case b&128 == 0:
-		return 1
-	case b&192 == 192 && b&32 == 0:
-		return 2
-	case b&224 == 224 && b&16 == 0:
-		return 3
-	case b&240 == 240 && b&8 == 0:
-		return 4
-	default:
-		return -1
-	}
+	return 0 == cnt
 }
