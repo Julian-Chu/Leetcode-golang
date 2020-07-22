f824faee8bf7839e5958a4a74227afb2c55fc227
yu-lang.chu <yu-lang.chu@redheads.de>
2020-07-22 21:12:26 +0200

    add leetcode405

--- leetcode405/leetcode405.go
+++ leetcode405/leetcode405.go
@@ -1,18 +1,16 @@
 package leetcode405
 
-import "strings"
-
 func toHex(num int) string {
 	if num == 0 {
 		return "0"
 	}
-
+	n := uint32(num)
 	idx := []byte{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'}
-	res := make([]byte, 8)
-	for i := 7; i >= 0; i-- {
-		res[i] = idx[num&15]
-		num = num >> 4
+	res := make([]byte, 0, 8)
+	for n != 0 {
+		res = append([]byte{idx[n&15]}, res...)
+		n >>= 4
 	}
 
-	return strings.TrimLeft(string(res), "0")
+	return string(res)
 }
