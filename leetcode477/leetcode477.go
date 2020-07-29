f3e4c10d45f211315456a191e32dff700d236db6
Julian Chu <yulang.chu@gmail.com>
2020-07-30 00:34:11 +0200

    add leetcode477

--- leetcode477/leetcode477.go
+++ leetcode477/leetcode477.go
@@ -1,27 +1,16 @@
 package leetcode477
 
 func totalHammingDistance(nums []int) int {
-	if len(nums) < 2 {
-		return 0
-	}
-	xor := make([]int, 1, len(nums)-1)
+	res := 0
+	n := len(nums)
 
-	xor[0] = nums[0]
-	sum := 0
-	for i := 1; i < len(nums); i++ {
-		for j := range xor {
-			sum += HammingDistance(xor[j] ^ nums[i])
+	for i := 0; i < 32; i++ {
+		k := 0
+		for j := 0; j < n; j++ {
+			k += (nums[j] >> uint(i)) & 1
 		}
-		xor = append(xor, nums[i])
-	}
-	return sum
-}
 
-func HammingDistance(num int) int {
-	cnt := 0
-	for num > 0 {
-		cnt += num & 1
-		num >>= 1
+		res += k * (n - k)
 	}
-	return cnt
+	return res
 }
