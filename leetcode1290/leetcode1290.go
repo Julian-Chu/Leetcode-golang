66700cacbd391ad379dacef6c7609d67c891b67e
Julian Chu <yulang.chu@gmail.com>
2020-08-18 18:42:19 +0200

    update 1290

--- leetcode1290/leetcode1290.go
+++ leetcode1290/leetcode1290.go
@@ -13,12 +13,9 @@ type ListNode = utils.ListNode
 
 func getDecimalValue(head *ListNode) int {
 	res := 0
-
 	for head != nil {
-		res <<= 1
-		res += head.Val
+		res = (res << 1) + head.Val
 		head = head.Next
 	}
-
 	return res
 }
