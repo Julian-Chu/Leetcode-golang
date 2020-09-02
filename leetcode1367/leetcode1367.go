c5088974d0e8dd94348a7b6f1bf103d63a14db07
yu-lang.chu <yu-lang.chu@redheads.de>
2020-09-02 15:33:36 +0200

    add leetcode1367

---
+++ leetcode1367/leetcode1367.go
@@ -0,0 +1,42 @@
+package leetcode1367
+
+import "Leetcode-golang/utils"
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+type ListNode = utils.ListNode
+type TreeNode = utils.TreeNode
+
+func isSubPath(head *ListNode, root *TreeNode) bool {
+	if head == nil {
+		return true
+	}
+	if root == nil {
+		return false
+	}
+
+	return dfs(head, root) || isSubPath(head, root.Left) || isSubPath(head, root.Right)
+}
+
+func dfs(head *ListNode, root *TreeNode) bool {
+	if head == nil {
+		return true
+	}
+	if root == nil {
+		return false
+	}
+	return head.Val == root.Val && (dfs(head.Next, root.Left) || dfs(head.Next, root.Right))
+}
