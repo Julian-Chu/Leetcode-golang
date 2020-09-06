a4dab855a6c8a4fd13387c393257368a81e6ad3d
Julian Chu <yulang.chu@gmail.com>
2020-09-06 22:50:43 +0200

    add leetcode215(select algorithm)

--- leetcode215/leetcode215.go
+++ leetcode215/leetcode215.go
@@ -1,43 +1,31 @@
 package leetcode215
 
-import (
-	"container/heap"
-)
-
 func findKthLargest(nums []int, k int) int {
-	temp := maxHeap(nums)
-	h := &temp
-	heap.Init(h)
-	if k == 1 {
-		return nums[0]
+	l, r := 0, len(nums)-1
+
+	for l <= r {
+		newPivot := partition(l, r, nums)
+		if newPivot == k-1 {
+			return nums[newPivot]
+		} else if newPivot > k-1 {
+			r = newPivot - 1
+		} else {
+			l = newPivot + 1
+		}
+	}
+	return -1
+}
+
+func partition(l int, r int, nums []int) int {
+	pv := nums[r]
+	newPivot := l
+	for i := l; i < r; i++ {
+		if nums[i] > pv {
+			nums[i], nums[newPivot] = nums[newPivot], nums[i]
+			newPivot++
+		}
 	}
 
-	for i := 1; i < k; i++ {
-		heap.Remove(h, 0)
-	}
-	return (*h)[0]
-}
-
-type maxHeap []int
-
-func (m maxHeap) Len() int {
-	return len(m)
-}
-
-func (m maxHeap) Less(i, j int) bool {
-	return m[i] > m[j]
-}
-
-func (m maxHeap) Swap(i, j int) {
-	m[i], m[j] = m[j], m[i]
-}
-
-func (m *maxHeap) Push(x interface{}) {
-	*m = append(*m, x.(int))
-}
-
-func (m *maxHeap) Pop() interface{} {
-	res := (*m)[len(*m)-1]
-	*m = (*m)[0 : len(*m)-1]
-	return res
+	nums[r], nums[newPivot] = nums[newPivot], nums[r]
+	return newPivot
 }
