52d3d908ede9bbba0ec2f2312112e81823bf161e
yu-lang.chu <yu-lang.chu@redheads.de>
2020-09-14 19:16:38 +0200

    add leetcode215

--- leetcode215/leetcode215.go
+++ leetcode215/leetcode215.go
@@ -1,31 +1,31 @@
 package leetcode215
 
 func findKthLargest(nums []int, k int) int {
-	l, r := 0, len(nums)-1
+	lo, hi := 0, len(nums)-1
 
-	for l <= r {
-		newPivot := partition(l, r, nums)
-		if newPivot == k-1 {
-			return nums[newPivot]
-		} else if newPivot > k-1 {
-			r = newPivot - 1
-		} else {
-			l = newPivot + 1
+	for lo <= hi {
+		newPivot := partition(nums, lo, hi)
+		switch {
+		case newPivot == k-1:
+			return nums[k-1]
+		case newPivot > k-1:
+			hi = newPivot - 1
+		default:
+			lo = newPivot + 1
 		}
 	}
 	return -1
 }
 
-func partition(l int, r int, nums []int) int {
-	pv := nums[r]
-	newPivot := l
-	for i := l; i < r; i++ {
+func partition(nums []int, lo int, hi int) int {
+	pv := nums[hi]
+	newPivot := lo
+	for i := lo; i < hi; i++ {
 		if nums[i] > pv {
 			nums[i], nums[newPivot] = nums[newPivot], nums[i]
 			newPivot++
 		}
 	}
-
-	nums[r], nums[newPivot] = nums[newPivot], nums[r]
+	nums[hi], nums[newPivot] = nums[newPivot], nums[hi]
 	return newPivot
 }
