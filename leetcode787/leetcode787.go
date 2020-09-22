8d1fcc6fd9712d1bc10412d978abf5ac9500ea72
yu-lang.chu <yu-lang.chu@redheads.de>
2020-09-22 18:57:21 +0200

    add leetcode787

--- leetcode787/leetcode787.go
+++ leetcode787/leetcode787.go
@@ -1,52 +1,66 @@
 package leetcode787
 
+import "container/heap"
+
 func findCheapestPrice(n int, flights [][]int, src int, dst int, K int) int {
-	graph := make([][]int, n)
-	visited := make([]bool, n)
-
-	for i := 0; i < n; i++ {
-		graph[i] = make([]int, n)
+	fmap := make([][][]int, n)
+	for i := range flights {
+		src := flights[i][0]
+		fmap[src] = append(fmap[src], flights[i][1:])
 	}
 
-	for _, flight := range flights {
-		src := flight[0]
-		dst := flight[1]
-		price := flight[2]
-		graph[src][dst] = price
-	}
+	pq := make(PQ, 0, n)
 
-	var dfs func(src, k, price int) int
+	heap.Push(&pq, &city{
+		price:     0,
+		id:        src,
+		countdown: K + 1,
+	})
 
-	maxPrice := 100 * 10000
-	dfs = func(src, k, price int) int {
-		if src == dst {
-			return price
+	for len(pq) > 0 {
+		ct, _ := heap.Pop(&pq).(*city)
+		if ct.id == dst {
+			return ct.price
 		}
-		if k == 0 {
-			return maxPrice
-		}
-		min := maxPrice
-		for d := 0; d < n; d++ {
-			if graph[src][d] < 1 {
-				continue
-			}
-			if visited[d] {
-				continue
-			}
-			visited[src] = true
-			sum := price + graph[src][d]
-			sum = dfs(d, k-1, sum)
-			if sum < min {
-				min = sum
-			}
-			visited[d] = false
-		}
-		return min
-	}
 
-	price := dfs(src, K+1, 0)
-	if price >= maxPrice {
-		return -1
+		if ct.countdown > 0 {
+			nexts := fmap[ct.id]
+			for _, n := range nexts {
+				heap.Push(&pq, &city{
+					id:        n[0],
+					price:     ct.price + n[1],
+					countdown: ct.countdown - 1,
+				})
+			}
+		}
 	}
-	return price
+	return -1
+}
+
+type city struct {
+	id, price, countdown int
+}
+
+type PQ []*city
+
+func (pq PQ) Len() int {
+	return len(pq)
+}
+
+func (pq PQ) Less(i, j int) bool {
+	return pq[i].price < pq[j].price
+}
+
+func (pq PQ) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+}
+
+func (pq *PQ) Push(x interface{}) {
+	*pq = append(*pq, x.(*city))
+}
+
+func (pq *PQ) Pop() interface{} {
+	res := (*pq)[len(*pq)-1]
+	*pq = (*pq)[0 : len(*pq)-1]
+	return res
 }
