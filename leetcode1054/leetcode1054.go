ab97787805b9b0a211c05030f5b0a7933aa21f63
yu-lang.chu <yu-lang.chu@redheads.de>
2020-09-28 17:50:53 +0200

    add leetcode1054

--- leetcode1054/leetcode1054.go
+++ leetcode1054/leetcode1054.go
@@ -3,10 +3,14 @@ package leetcode1054
 import "sort"
 
 func rearrangeBarcodes(barcodes []int) []int {
-	n := len(barcodes)
+	if len(barcodes) == 0 {
+		return []int{}
+	}
+
 	cnt := [10001]int{}
-	for _, b := range barcodes {
-		cnt[b]++
+
+	for _, bc := range barcodes {
+		cnt[bc]++
 	}
 
 	sort.Slice(barcodes, func(i, j int) bool {
@@ -16,12 +20,12 @@ func rearrangeBarcodes(barcodes []int) []int {
 		return cnt[barcodes[i]] > cnt[barcodes[j]]
 	})
 
-	res := make([]int, n)
+	res := make([]int, len(barcodes))
 	i := 0
-	for _, b := range barcodes {
-		res[i] = b
+	for _, bc := range barcodes {
+		res[i] = bc
 		i += 2
-		if i > n-1 {
+		if i >= len(barcodes) {
 			i = 1
 		}
 	}
