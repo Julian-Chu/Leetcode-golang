bcaadb4b43f4f80f5b95e53736f30f18a7795029
yu-lang.chu <yu-lang.chu@redheads.de>
2020-10-15 18:42:16 +0200

    add leetcode1202

--- leetcode1202/leetcode1202.go
+++ leetcode1202/leetcode1202.go
@@ -3,46 +3,57 @@ package leetcode1202
 import "sort"
 
 func smallestStringWithSwaps(s string, pairs [][]int) string {
-	n := len(s)
-	parent := make([]int, n)
-	for i := range parent {
-		parent[i] = i
+	dsu := make(Dsu, len(s))
+	dsu.init()
+	for _, pair := range pairs {
+		dsu.Union(pair[0], pair[1])
 	}
 
-	for _, p := range pairs {
-		xp, yp := getParent(p[0], parent), getParent(p[1], parent)
-		parent[xp] = yp
-	}
-
-	m := make(map[int][]int)
+	m := make(map[int][]byte)
 
 	for i := range s {
-		p := getParent(i, parent)
-		m[p] = append(m[p], i)
+		b := s[i]
+		p := dsu.Parent(i)
+		m[p] = append(m[p], b)
 	}
 
-	bs := []byte(s)
-	res := make([]byte, n)
-	for _, g := range m {
-		idx := make([]int, len(g))
-		copy(idx, g)
-		sort.Slice(idx, func(i, j int) bool {
-			return bs[idx[i]] < bs[idx[j]]
+	for k, v := range m {
+		sort.Slice(v, func(i, j int) bool {
+			return v[i] < v[j]
 		})
-
-		sort.Ints(g)
-		for i := range g {
-			res[g[i]] = bs[idx[i]]
-		}
+		m[k] = v
 	}
-	return string(res)
+
+	ret := make([]byte, len(s))
+	for i := range ret {
+		p := dsu.Parent(i)
+		group := m[p]
+		ret[i] = group[0]
+		m[p] = group[1:]
+	}
+	return string(ret)
 
 }
 
-func getParent(i int, parent []int) int {
-	if i == parent[i] {
-		return i
-	}
+type Dsu []int
 
-	return getParent(parent[i], parent)
+func (d Dsu) init() {
+	for i := range d {
+		d[i] = i
+	}
+}
+
+func (d Dsu) Parent(x int) int {
+	for x != d[x] {
+		x, d[x] = d[x], d[d[x]]
+	}
+	return x
+}
+
+func (d Dsu) Union(x, y int) {
+	xp, yp := d.Parent(x), d.Parent(y)
+	if xp == yp {
+		return
+	}
+	d[xp] = yp
 }
