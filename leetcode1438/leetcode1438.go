0eb843028765cf07cda6d2243a826e8d9de22330
yu-lang.chu <yu-lang.chu@redheads.de>
2020-10-30 22:33:00 +0100

    add leetcode1438

--- leetcode1438/leetcode1438.go
+++ leetcode1438/leetcode1438.go
@@ -1,35 +1,39 @@
 package leetcode1438
 
 func longestSubarray(nums []int, limit int) int {
-	n := len(nums)
-	maxd := make([]int, 0, n)
-	mind := make([]int, 0, n)
-	i := 0
-
-	for _, num := range nums {
-		for len(maxd) > 0 && num > maxd[len(maxd)-1] {
-			maxd = maxd[:len(maxd)-1]
-		}
-		for len(mind) > 0 && num < mind[len(mind)-1] {
-			mind = mind[:len(mind)-1]
-		}
-
-		maxd = append(maxd, num)
-		mind = append(mind, num)
-
-		if maxd[0]-mind[0] > limit {
-			if maxd[0] == nums[i] {
-				copy(maxd, maxd[1:])
-				maxd = maxd[:len(maxd)-1]
-			}
-
-			if mind[0] == nums[i] {
-				copy(mind, mind[1:])
-				mind = mind[:len(mind)-1]
-			}
-
-			i++
-		}
+	l := len(nums)
+	if l == 0 {
+		return 0
 	}
-	return n - i
+	var s, e, ans int = 0, 1, 1
+	var min, max []int = []int{0}, []int{0}
+
+	for e < l {
+		for len(min) > 0 && nums[e] < nums[min[len(min)-1]] {
+			min = min[:len(min)-1]
+		}
+
+		for len(max) > 0 && nums[e] > nums[max[len(max)-1]] {
+			max = max[:len(max)-1]
+		}
+
+		min = append(min, e)
+		max = append(max, e)
+
+		for nums[max[0]]-nums[min[0]] > limit {
+			s++
+			if min[0] < s {
+				min = min[1:]
+			}
+			if max[0] < s {
+				max = max[1:]
+			}
+		}
+
+		if ans < e-s+1 {
+			ans = e - s + 1
+		}
+		e++
+	}
+	return ans
 }
