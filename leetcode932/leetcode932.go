661ceed29b07f7548d7cd00f1588c4a7fdbd4c34
yu-lang.chu <yu-lang.chu@redheads.de>
2020-11-03 15:22:32 +0100

    add leetcode932

---
+++ leetcode932/leetcode932.go
@@ -0,0 +1,21 @@
+package leetcode932
+
+func beautifulArray(N int) []int {
+	res := []int{1}
+	for len(res) < N {
+		tmp := make([]int, 0, len(res)*2)
+		for _, v := range res {
+			if v*2-1 <= N {
+				tmp = append(tmp, v*2-1)
+			}
+		}
+
+		for _, v := range res {
+			if v*2 <= N {
+				tmp = append(tmp, v*2)
+			}
+		}
+		res = tmp
+	}
+	return res
+}
