2d2f4d2b0a349730d1e9a2346f9e69dcb82b27c0
Julian Chu <yulang.chu@gmail.com>
2020-11-07 22:59:18 +0100

    add leetcode720

--- leetcode720/leetcode720.go
+++ leetcode720/leetcode720.go
@@ -3,28 +3,20 @@ package leetcode720
 import "sort"
 
 func longestWord(words []string) string {
-	m := make(map[string]bool)
+	sort.Strings(words)
+	res := ""
+	set := make(map[string]bool, len(words))
 
 	for _, word := range words {
-		m[word] = true
-	}
-
-	sort.Slice(words, func(i, j int) bool {
-		if len(words[i]) == len(words[j]) {
-			return words[i] < words[j]
+		if len(word) > 1 && !set[word[:len(word)-1]] {
+			continue
 		}
-		return len(words[i]) > len(words[j])
-	})
 
-	for _, word := range words {
-		for i := range word {
-			if _, ok := m[word[:i+1]]; !ok {
-				break
-			}
-			if i == len(word)-1 {
-				return word
-			}
+		set[word] = true
+
+		if len(word) > len(res) {
+			res = word
 		}
 	}
-	return ""
+	return res
 }
