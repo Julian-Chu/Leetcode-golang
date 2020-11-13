d768443e976950154906139080c73ce7b33848b2
yu-lang.chu <yu-lang.chu@redheads.de>
2020-11-13 13:19:04 +0100

    add leetcode779

---
+++ leetcode779/leetcode779.go
@@ -0,0 +1,13 @@
+package leetcode779
+
+func kthGrammar(N int, K int) int {
+	if N == 1 {
+		return 0
+	}
+
+	if K&1 == 1 {
+		return kthGrammar(N-1, (K+1)/2)
+	}
+
+	return kthGrammar(N-1, K/2) ^ 1
+}
