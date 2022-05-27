599329825d4e514283f092fde6a4d5fd724d6833
Yu-Lang Chu <yulang.chu@gmail.com>
2022-05-27 15:26:32 +0200

    leetcode1710, bucket sort solution

--- leetcode/Leetcode_1710_MaximumUnitsOnATruck/leetcode1710.go
+++ leetcode/Leetcode_1710_MaximumUnitsOnATruck/leetcode1710.go
@@ -1,31 +1,48 @@
 package Leetcode_1710_MaximumUnitsOnATruck
 
-import (
-	"sort"
-)
-
 func maximumUnits(boxTypes [][]int, truckSize int) int {
-	sort.Slice(boxTypes, func(i, j int) bool {
-		if boxTypes[i][1] > boxTypes[j][1] {
-			return true
-		}
-		return false
-	})
+	buckets := make([]int, 1001)
 
-	var cnt = 0
-
-	for _, boxType := range boxTypes {
-		if truckSize <= 0 {
-			break
-		}
-		totalNum := boxType[0]
-		unitsPerBox := boxType[1]
-		num := totalNum
-		if truckSize < num {
-			num = truckSize
-		}
-		cnt += num * unitsPerBox
-		truckSize -= num
+	for _, box := range boxTypes {
+		buckets[box[1]] += box[0]
 	}
-	return cnt
+
+	units := 0
+
+	for i := 1000; i > 0; i-- {
+		if truckSize >= buckets[i] {
+			units += buckets[i] * i
+			truckSize -= buckets[i]
+		} else {
+			units += truckSize * i
+			return units
+		}
+	}
+	return units
 }
+
+//func maximumUnits(boxTypes [][]int, truckSize int) int {
+//	sort.Slice(boxTypes, func(i, j int) bool {
+//		if boxTypes[i][1] > boxTypes[j][1] {
+//			return true
+//		}
+//		return false
+//	})
+//
+//	var cnt = 0
+//
+//	for _, boxType := range boxTypes {
+//		if truckSize <= 0 {
+//			break
+//		}
+//		totalNum := boxType[0]
+//		unitsPerBox := boxType[1]
+//		num := totalNum
+//		if truckSize < num {
+//			num = truckSize
+//		}
+//		cnt += num * unitsPerBox
+//		truckSize -= num
+//	}
+//	return cnt
+//}
