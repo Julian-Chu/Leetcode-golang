6071ba4950d3e99889a77c4473407a135f0824c9
Yu-Lang Chu <yulang.chu@gmail.com>
2022-05-29 11:52:56 +0200

    update leetcode35

--- leetcode/leetcode35/leetcode35.go
+++ leetcode/leetcode35/leetcode35.go
@@ -1,24 +1,53 @@
 package leetcode35
 
 func searchInsert(nums []int, target int) int {
-	n := len(nums)
-	l, r := 0, n-1
-	var mid int
-	for l <= r {
-		mid = (l + r) / 2
+	if nums == nil || len(nums) == 0 {
+		return 0
+	}
+
+	start, end := 0, len(nums)-1
+	for start+1 < end {
+		mid := start + (end-start)>>1
 		switch {
+		case nums[mid] == target:
+			end = mid
 		case nums[mid] < target:
-			l = mid + 1
-		case nums[mid] > target:
-			r = mid - 1
+			start = mid
 		default:
-			return mid
+			end = mid
 		}
 	}
 
-	if target > nums[mid] {
-		return mid + 1
+	if nums[start] >= target {
+		return start
 	}
 
-	return mid
+	if nums[end] < target {
+		return end + 1
+	}
+
+	return end
 }
+
+//func searchInsert(nums []int, target int) int {
+//	n := len(nums)
+//	l, r := 0, n-1
+//	var mid int
+//	for l <= r {
+//		mid = (l + r) / 2
+//		switch {
+//		case nums[mid] < target:
+//			l = mid + 1
+//		case nums[mid] > target:
+//			r = mid - 1
+//		default:
+//			return mid
+//		}
+//	}
+//
+//	if target > nums[mid] {
+//		return mid + 1
+//	}
+//
+//	return mid
+//}
