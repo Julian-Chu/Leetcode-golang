4ed28ea7b2fbcca9cd82f38855cff713ca1cbd23
Yu-Lang Chu <yulang.chu@gmail.com>
2022-05-31 10:54:50 +0200

    add leetcode904 solution 2

--- leetcode/LeetCode_904_FruistIntoBaskets/leetcode904.go
+++ leetcode/LeetCode_904_FruistIntoBaskets/leetcode904.go
@@ -4,35 +4,62 @@ func totalFruit(fruits []int) int {
 	if len(fruits) <= 1 {
 		return len(fruits)
 	}
-	slow, fast := 0, 0
-	sum, res := 0, 0
-	set := make(map[int]int)
-
-	for slow <= fast && fast < len(fruits) {
-		fruit := fruits[fast]
-		_, exist := set[fruit]
-
-		if exist || len(set) < 2 {
-			if exist {
-				set[fruit]++
-			} else {
-				set[fruit] = 1
-			}
-			sum++
-			if res < sum {
-				res = sum
-			}
-			fast++
-			continue
+	lastFruit, lastFruitCount := -1, 0
+	firstFruit := 0
+	count := 0
+	res := 0
+	for _, t := range fruits {
+		count++
+		if t != firstFruit && t != lastFruit {
+			count = lastFruitCount + 1
 		}
 
-		set[fruits[slow]]--
-		if set[fruits[slow]] == 0 {
-			delete(set, fruits[slow])
+		if res < count {
+			res = count
 		}
 
-		sum--
-		slow++
+		lastFruitCount++
+		if t != lastFruit {
+			firstFruit, lastFruit = lastFruit, t
+			lastFruitCount = 1
+		}
 	}
 	return res
 }
+
+//func totalFruit(fruits []int) int {
+//	if len(fruits) <= 1 {
+//		return len(fruits)
+//	}
+//	slow, fast := 0, 0
+//	sum, res := 0, 0
+//	set := make(map[int]int)
+//
+//	for slow <= fast && fast < len(fruits) {
+//		fruit := fruits[fast]
+//		_, exist := set[fruit]
+//
+//		if exist || len(set) < 2 {
+//			if exist {
+//				set[fruit]++
+//			} else {
+//				set[fruit] = 1
+//			}
+//			sum++
+//			if res < sum {
+//				res = sum
+//			}
+//			fast++
+//			continue
+//		}
+//
+//		set[fruits[slow]]--
+//		if set[fruits[slow]] == 0{
+//			delete(set, fruits[slow])
+//		}
+//
+//		sum--
+//		slow++
+//	}
+//	return res
+//}
