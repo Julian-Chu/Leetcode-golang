6e3bcfe4cbda1791f142c40675da1403f58cca5c
Yu-Lang Chu <yulang.chu@gmail.com>
2022-06-02 13:50:13 +0200

    update leetcode349

--- leetcode/LeetCode_349_IntersectionOfTwoArrays/leetcode349.go
+++ leetcode/LeetCode_349_IntersectionOfTwoArrays/leetcode349.go
@@ -5,29 +5,24 @@ func intersection(nums1 []int, nums2 []int) []int {
 		nums1, nums2 = nums2, nums1
 	}
 
-	m := make(map[int]bool)
+	m := make(map[int]struct{})
 
 	for _, key := range nums1 {
 		if _, ok := m[key]; ok {
 			continue
 		}
-		m[key] = false
+		m[key] = struct{}{}
 	}
 
+	res := make([]int, 0, len(m))
 	for _, key := range nums2 {
 		if _, ok := m[key]; ok {
-			m[key] = true
+			res = append(res, key)
+			delete(m, key)
 			continue
 		}
 	}
 
-	res := make([]int, 0, len(m))
-
-	for key, v := range m {
-		if v == true {
-			res = append(res, key)
-		}
-	}
 	return res
 }
 
