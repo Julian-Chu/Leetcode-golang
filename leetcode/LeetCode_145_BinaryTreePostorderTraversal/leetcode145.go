be867c8c0683bd20bd8dfdb3fa11a4dae8a59fd5
Yu-Lang Chu <yulang.chu@gmail.com>
2022-06-05 13:40:12 +0200

    update leetcode145

--- leetcode/LeetCode_145_BinaryTreePostorderTraversal/leetcode145.go
+++ leetcode/LeetCode_145_BinaryTreePostorderTraversal/leetcode145.go
@@ -54,7 +54,33 @@ func postorderTraversal_stack(root *TreeNode) []int {
 		i++
 		j--
 	}
-
 	return res
+}
 
+func postorderTraversal_template(root *TreeNode) []int {
+	res := make([]int, 0)
+	if root == nil {
+		return res
+	}
+
+	stack := []*TreeNode{root}
+	for len(stack) > 0 {
+		node := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		if node != nil {
+			stack = append(stack, node)
+			stack = append(stack, nil)
+			if node.Right != nil {
+				stack = append(stack, node.Right)
+			}
+			if node.Left != nil {
+				stack = append(stack, node.Left)
+			}
+		} else {
+			cur := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			res = append(res, cur.Val)
+		}
+	}
+	return res
 }
