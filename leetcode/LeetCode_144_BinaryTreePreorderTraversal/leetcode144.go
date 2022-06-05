daf233ab5ee49e82c9366d3a58f54e7d37430834
Yu-Lang Chu <yulang.chu@gmail.com>
2022-06-05 13:35:59 +0200

    add leetcode144

--- leetcode/LeetCode_144_BinaryTreePreorderTraversal/leetcode144.go
+++ leetcode/LeetCode_144_BinaryTreePreorderTraversal/leetcode144.go
@@ -63,6 +63,39 @@ func preorderTraversal_stack2(root *TreeNode) []int {
 	return res
 }
 
+func preorderTraversal_template(root *TreeNode) []int {
+	res := make([]int, 0)
+
+	if root == nil {
+		return res
+	}
+
+	stack := []*TreeNode{root}
+
+	for len(stack) > 0 {
+		node := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		if node != nil {
+			if node.Right != nil {
+				stack = append(stack, node.Right)
+			}
+			if node.Left != nil {
+				stack = append(stack, node.Left)
+			}
+
+			stack = append(stack, node)
+			stack = append(stack, nil)
+		} else {
+			cur := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			res = append(res, cur.Val)
+		}
+	}
+	return res
+
+}
+
 func preorderTraversal(root *TreeNode) []int {
 	res := make([]int, 0)
 	var traverse func(node *TreeNode)
