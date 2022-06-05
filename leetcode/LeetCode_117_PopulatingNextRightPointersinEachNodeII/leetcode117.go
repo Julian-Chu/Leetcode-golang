d21d809a38e0370ade536512f739d4a34da56c2f
Yu-Lang Chu <yulang.chu@gmail.com>
2022-06-05 23:44:56 +0200

    add leetcode117

---
+++ leetcode/LeetCode_117_PopulatingNextRightPointersinEachNodeII/leetcode117.go
@@ -0,0 +1,39 @@
+package LeetCode_117_PopulatingNextRightPointersinEachNodeII
+
+type Node struct {
+	Val   int
+	Left  *Node
+	Right *Node
+	Next  *Node
+}
+
+func connect(root *Node) *Node {
+	if root == nil {
+		return nil
+	}
+
+	queue := []*Node{root}
+
+	for len(queue) > 0 {
+		size := len(queue)
+		var prev *Node
+		for i := 0; i < size; i++ {
+			cur := queue[i]
+			if cur.Left != nil {
+				queue = append(queue, cur.Left)
+			}
+			if cur.Right != nil {
+				queue = append(queue, cur.Right)
+			}
+			if prev != nil {
+				prev.Next = cur
+			}
+			prev = cur
+
+		}
+
+		queue = queue[size:]
+	}
+
+	return root
+}
