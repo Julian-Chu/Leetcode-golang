f41f6414da5c3e7d88da3415d976adc635de9f1b
Yu-Lang Chu <yulang.chu@gmail.com>
2022-06-06 00:01:43 +0200

    update leetcode226

--- leetcode/LeetCode_226_InvertBinaryTree/226.go
+++ leetcode/LeetCode_226_InvertBinaryTree/226.go
@@ -18,3 +18,37 @@ func invertNodes(root *utils.TreeNode) {
 	invertNodes(root.Right)
 	root.Left, root.Right = root.Right, root.Left
 }
+
+type TreeNode struct {
+	Val         int
+	Left, Right *TreeNode
+}
+
+func invertTree_DFS(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	stack := []*TreeNode{root}
+
+	for len(stack) > 0 {
+		node := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		if node != nil {
+			if node.Right != nil {
+				stack = append(stack, node.Right)
+			}
+			if node.Left != nil {
+				stack = append(stack, node.Left)
+			}
+			stack = append(stack, node)
+			stack = append(stack, nil)
+		} else {
+			cur := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			cur.Left, cur.Right = cur.Right, cur.Left
+		}
+	}
+	return root
+}
