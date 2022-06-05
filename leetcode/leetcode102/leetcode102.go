8fc78e53e8f866b983305db41d1ec69ed8c93917
Yu-Lang Chu <yulang.chu@gmail.com>
2022-06-05 13:50:29 +0200

    update leetcode102

--- leetcode/leetcode102/leetcode102.go
+++ leetcode/leetcode102/leetcode102.go
@@ -24,3 +24,33 @@ func BuildLevelOrderTraversal(node *TreeNode, level int, res *[][]int) {
 	BuildLevelOrderTraversal(node.Left, level+1, res)
 	BuildLevelOrderTraversal(node.Right, level+1, res)
 }
+
+// BFS, better solution
+func levelOrder_1(root *TreeNode) [][]int {
+	queue := make([]*TreeNode, 0)
+	res := make([][]int, 0)
+	if root == nil {
+		return res
+	}
+
+	queue = append(queue, root)
+
+	for len(queue) > 0 {
+		size := len(queue)
+		nodes := make([]int, 0, size)
+		for i := 0; i < size; i++ {
+			cur := queue[i]
+			nodes = append(nodes, cur.Val)
+			if cur.Left != nil {
+				queue = append(queue, cur.Left)
+			}
+			if cur.Right != nil {
+				queue = append(queue, cur.Right)
+			}
+		}
+		res = append(res, nodes)
+		queue = queue[size:]
+	}
+
+	return res
+}
