977e25896e5c68b784d886d645ab05854616d0bf
Yu-Lang Chu <yulang.chu@gmail.com>
2022-06-06 14:46:06 +0200

    update leetcode222

--- leetcode/leetcode222/leetcode222.go
+++ leetcode/leetcode222/leetcode222.go
@@ -20,3 +20,30 @@ func countNodes(root *TreeNode) int {
 
 	return countNodes(root.Right) + countNodes(root.Left) + 1
 }
+
+func countNodes_completeBTree(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	l := root.Left
+	r := root.Right
+
+	lDep := 0
+	for l != nil {
+		lDep++
+		l = l.Left
+	}
+	rDep := 0
+	for r != nil {
+		rDep++
+		r = r.Right
+	}
+
+	// complete binary tree
+	if lDep == rDep {
+		return 2<<lDep - 1
+	}
+
+	return countNodes(root.Left) + countNodes(root.Right) + 1
+}
