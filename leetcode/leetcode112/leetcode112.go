d82b209c6fef2209d71b7ce03032f9ca3466b9bb
Yu-Lang Chu <yulang.chu@gmail.com>
2022-06-06 21:18:30 +0200

    update leetcode112

--- leetcode/leetcode112/leetcode112.go
+++ leetcode/leetcode112/leetcode112.go
@@ -17,3 +17,25 @@ func hasPathSum(root *TreeNode, sum int) bool {
 	sum -= root.Val
 	return hasPathSum(root.Left, sum) || hasPathSum(root.Right, sum)
 }
+
+func hasPathSum_1(root *TreeNode, targetSum int) bool {
+
+	var dfs func(node *TreeNode, sum int) bool
+
+	dfs = func(node *TreeNode, sum int) bool {
+		if node == nil {
+			return false
+		}
+
+		sum += node.Val
+		if node.Left == nil && node.Right == nil {
+			if sum == targetSum {
+				return true
+			}
+		}
+
+		return dfs(node.Left, sum) || dfs(node.Right, sum)
+	}
+
+	return dfs(root, 0)
+}
