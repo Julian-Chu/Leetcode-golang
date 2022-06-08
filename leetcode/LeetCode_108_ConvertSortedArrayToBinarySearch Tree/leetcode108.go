7c77dfea48c3bc49eabad09d4282ba1945432f7a
Yu-Lang Chu <yulang.chu@gmail.com>
2022-06-08 10:46:38 +0200

    update leetcode108

--- leetcode/LeetCode_108_ConvertSortedArrayToBinarySearch Tree/leetcode108.go
+++ leetcode/LeetCode_108_ConvertSortedArrayToBinarySearch Tree/leetcode108.go
@@ -1,4 +1,4 @@
-package leetcode108
+package LeetCode_108_ConvertSortedArrayToBinarySearch_Tree
 
 import "github.com/Julian-Chu/Leetcode-golang/utils"
 
@@ -20,5 +20,19 @@ func dfs(nums []int, l, r int) *TreeNode {
 
 	root.Left = dfs(nums, l, mid-1)
 	root.Right = dfs(nums, mid+1, r)
+	return root
+}
+
+func sortedArrayToBST_1(nums []int) *TreeNode {
+	size := len(nums)
+	if size == 0 {
+		return nil
+	}
+
+	mid := size / 2
+
+	root := &TreeNode{Val: nums[mid]}
+	root.Left = sortedArrayToBST(nums[:mid])
+	root.Right = sortedArrayToBST(nums[mid+1:])
 	return root
 }
