cf7789974beaa4d0c85428f05ff200160bf9e17e
Yu-Lang Chu <yulang.chu@gmail.com>
2022-06-08 10:05:24 +0200

    add leetcode669

---
+++ leetcode/LeetCode_669_TrimABinarySearchTree/leetcode669.go
@@ -0,0 +1,58 @@
+package LeetCode_669_TrimABinarySearchTree
+
+import "github.com/Julian-Chu/Leetcode-golang/utils"
+
+type TreeNode = utils.TreeNode
+
+func trimBST(root *TreeNode, low int, high int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	if root.Val >= low && root.Val <= high {
+		root.Left = trimBST(root.Left, low, high)
+		root.Right = trimBST(root.Right, low, high)
+		return root
+	}
+
+	if root.Val < low {
+		return trimBST(root.Right, low, high)
+	} else if root.Val > high {
+		return trimBST(root.Left, low, high)
+	}
+
+	return nil
+
+}
+
+func trimBST_1(root *TreeNode, low int, high int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	for root != nil && (root.Val < low || root.Val > high) {
+		if root.Val < low {
+			root = root.Right
+		} else {
+			root = root.Left
+		}
+	}
+
+	cur := root
+
+	for cur != nil {
+		for cur.Left != nil && cur.Left.Val < low {
+			cur.Left = cur.Left.Right
+		}
+		cur = cur.Left
+	}
+	cur = root
+
+	for cur != nil {
+		for cur.Right != nil && cur.Right.Val > high {
+			cur.Right = cur.Right.Left
+		}
+		cur = cur.Right
+	}
+	return root
+}
