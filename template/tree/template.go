8ebcdf0acfd6931abe16a970568e8edaca95cec5
Yu-Lang Chu <yulang.chu@gmail.com>
2022-06-13 01:56:47 +0200

    update template

--- template/tree/template.go
+++ template/tree/template.go
@@ -28,3 +28,63 @@ func inorderTraversal_template(root *TreeNode) []int {
 	}
 	return res
 }
+
+func preorderTraversal_template(root *TreeNode) []int {
+	res := make([]int, 0)
+	if root == nil {
+		return res
+	}
+	stack := []*TreeNode{root}
+
+	for len(stack) > 0 {
+		node := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		if node != nil {
+			if node.Right != nil {
+				stack = append(stack, node.Right)
+			}
+
+			if node.Left != nil {
+				stack = append(stack, node.Left)
+			}
+
+			stack = append(stack, node)
+			stack = append(stack, nil)
+		} else {
+			cur := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			res = append(res, cur.Val)
+		}
+	}
+	return res
+}
+
+func postorderTraversal_template(root *TreeNode) []int {
+	res := make([]int, 0)
+	if root == nil {
+		return res
+	}
+	stack := []*TreeNode{root}
+
+	for len(stack) > 0 {
+		node := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		if node != nil {
+			stack = append(stack, node)
+			stack = append(stack, nil)
+
+			if node.Right != nil {
+				stack = append(stack, node.Right)
+			}
+
+			if node.Left != nil {
+				stack = append(stack, node.Left)
+			}
+		} else {
+			cur := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			res = append(res, cur.Val)
+		}
+	}
+	return res
+}
