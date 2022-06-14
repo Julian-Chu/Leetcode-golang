e4a6069a44dd2f852cb93390e9f46f70d2792f65
Yu-Lang Chu <yulang.chu@gmail.com>
2022-06-14 17:36:54 +0200

    update leetcode63

--- leetcode/leetcode63/leetcode63.go
+++ leetcode/leetcode63/leetcode63.go
@@ -37,3 +37,33 @@ func uniquePathsWithObstacles(obstacleGrid [][]int) int {
 
 	return res[m][n]
 }
+
+func uniquePathsWithObstacles_1(grid [][]int) int {
+	m := len(grid)
+	n := len(grid[0])
+
+	dp := make([][]int, m)
+
+	for i := range dp {
+		dp[i] = make([]int, n)
+	}
+
+	for j := 0; j < n && grid[0][j] == 0; j++ {
+		dp[0][j] = 1
+	}
+
+	for i := 0; i < m && grid[i][0] == 0; i++ {
+		dp[i][0] = 1
+	}
+
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			dp[i][j] = dp[i-1][j] + dp[i][j-1]
+			if grid[i][j] == 1 {
+				dp[i][j] = 0
+			}
+		}
+	}
+
+	return dp[m-1][n-1]
+}
