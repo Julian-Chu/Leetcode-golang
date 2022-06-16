01e5ec852b0e13551195f14172407106143aa852
Yu-Lang Chu <yulang.chu@gmail.com>
2022-06-16 21:19:04 +0200

    update leetcode279

--- leetcode/leetcode279/leetcode279.go
+++ leetcode/leetcode279/leetcode279.go
@@ -27,3 +27,27 @@ func isSquare(n int) bool {
 
 	return c*c == n
 }
+
+func numSquares_1(n int) int {
+	dp := make([]int, n+1)
+	dp[0] = 0
+
+	for i := 1; i < len(dp); i++ {
+		dp[i] = 1<<31 - 1
+	}
+
+	for i := 1; i <= n; i++ {
+		for j := i * i; j <= n; j++ {
+			dp[j] = min(dp[j], dp[j-i*i]+1)
+		}
+	}
+
+	return dp[n]
+}
+
+func min(a, b int) int {
+	if a <= b {
+		return a
+	}
+	return b
+}
