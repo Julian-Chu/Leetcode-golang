af1a942d0233a40d8bb2d010a10a4b59c94af36a
Yu-Lang Chu <yulang.chu@gmail.com>
2022-06-17 13:24:02 +0200

    update leetcode213

--- leetcode/leetcode213/leetcode213.go
+++ leetcode/leetcode213/leetcode213.go
@@ -34,3 +34,45 @@ func rob(nums []int) int {
 	}
 	return max
 }
+
+func rob_1(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	return max(robInternal(nums[1:]), robInternal(nums[:len(nums)-1]))
+}
+
+func robInternal(nums []int) int {
+	n := len(nums)
+	if n < 1 {
+		return 0
+	}
+
+	if n == 1 {
+		return nums[0]
+	}
+
+	if n == 2 {
+		return max(nums[0], nums[1])
+	}
+
+	dp := make([]int, n)
+	dp[0] = nums[0]
+	dp[1] = max(nums[0], nums[1])
+
+	for i := 2; i < n; i++ {
+		dp[i] = max(dp[i-1], dp[i-2]+nums[i])
+	}
+
+	return dp[n-1]
+}
+
+func max(a, b int) int {
+	if a >= b {
+		return a
+	}
+	return b
+}
