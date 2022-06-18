6a8e8228b9d40bcd6105bd3cfe9ffb0974ca9e31
Yu-Lang Chu <yulang.chu@gmail.com>
2022-06-18 17:48:29 +0200

    update leetcode714

--- leetcode/LeetCode_714_BestTimeToBuyAndSellStockWithTransactionFee/leetcode714.go
+++ leetcode/LeetCode_714_BestTimeToBuyAndSellStockWithTransactionFee/leetcode714.go
@@ -21,3 +21,25 @@ func maxProfit(prices []int, fee int) int {
 	}
 	return res
 }
+
+func maxProfit_dp(prices []int, fee int) int {
+	n := len(prices)
+	dp := [2][]int{}
+	dp[0] = make([]int, n) // with stock
+	dp[1] = make([]int, n) // without stock
+
+	dp[0][0] -= prices[0]
+	for i := 1; i < n; i++ {
+		dp[0][i] = max(dp[0][i-1], dp[1][i-1]-prices[i])
+		dp[1][i] = max(dp[1][i-1], dp[0][i-1]+prices[i]-fee)
+	}
+
+	return max(dp[0][n-1], dp[1][n-1])
+}
+
+func max(a, b int) int {
+	if a >= b {
+		return a
+	}
+	return b
+}
