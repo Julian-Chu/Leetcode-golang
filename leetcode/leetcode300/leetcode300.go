1dde15cff15fd1bdd67befbec6597b3e06eebade
Yu-Lang Chu <yulang.chu@gmail.com>
2022-06-18 18:30:17 +0200

    update leetcode300

--- leetcode/leetcode300/leetcode300.go
+++ leetcode/leetcode300/leetcode300.go
@@ -1,6 +1,9 @@
 package leetcode300
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func lengthOfLIS(nums []int) int {
 	res := make([]int, len(nums))
@@ -24,49 +27,51 @@ func max(a int, b int) int {
 	return b
 }
 
-//func lengthOfLIS(nums []int) int {
-//	res := make([]int, 0, len(nums))
-//	for _, v := range nums {
-//		at := sort.SearchInts(res, v)
-//		if at == len(res) {
-//			res = append(res, v)
-//		} else if res[at] > v {
-//			res[at] = v
-//		}
-//	}
-//	return len(res)
-//}
+func lengthOfLIS_1(nums []int) int {
+	res := make([]int, 0, len(nums))
+	for _, v := range nums {
+		at := sort.SearchInts(res, v)
+		if at == len(res) {
+			res = append(res, v)
+		} else if res[at] > v {
+			res[at] = v
+		}
+	}
+	return len(res)
+}
 
-//func lengthOfLIS(nums []int) int {
-//	if len(nums) == 0 {
-//		return 0
-//	}
-//	res := []int{nums[0]}
-//OUTER:
-//	for i := 1; i < len(nums); i++ {
-//		for _, v := range res {
-//			if v == nums[i] {
-//				continue OUTER
-//			}
-//		}
-//		if nums[i] < res[0] {
-//			res[0] = nums[i]
-//		} else if nums[i] > res[len(res)-1] {
-//			res = append(res, nums[i])
-//		} else {
-//			l, r := 0, len(res)-1
-//			mid := (l + r) / 2
-//			for r-l > 1 {
-//				if nums[i] > res[mid] {
-//					l = mid
-//				} else {
-//					r = mid
-//				}
-//				mid = (l + r) / 2
-//			}
-//			res[r] = nums[i]
-//		}
-//	}
-//	//fmt.Printf("%v", res)
-//	return len(res)
-//}
+func lengthOfLIS_2(nums []int) int {
+	sub := []int{}
+	for _, num := range nums {
+		if len(sub) == 0 || sub[len(sub)-1] < num {
+			sub = append(sub, num)
+		} else {
+			idx := bisect(sub, num)
+			sub[idx] = num
+		}
+	}
+	return len(sub)
+
+}
+
+func bisect(sub []int, num int) int {
+	if len(sub) == 0 {
+		return 0
+	}
+
+	start, end := 0, len(sub)-1
+
+	for start+1 < end {
+		mid := start + (end-start)>>1
+		if sub[mid] >= num {
+			end = mid
+		} else {
+			start = mid
+		}
+	}
+
+	if sub[start] >= num {
+		return start
+	}
+	return end
+}
