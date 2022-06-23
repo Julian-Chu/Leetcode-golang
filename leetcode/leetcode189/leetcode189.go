8cbd0f37a7a02ba94d4c2fee9a3448dbc59b7b98
Yu-Lang Chu <yulang.chu@gmail.com>
2022-06-23 22:32:46 +0200

    update leetcode189

--- leetcode/leetcode189/leetcode189.go
+++ leetcode/leetcode189/leetcode189.go
@@ -7,3 +7,22 @@ func rotate(nums []int, k int) {
 
 	copy(nums, append(nums[len(nums)-k:], nums[:len(nums)-k]...))
 }
+
+func rotate_1(nums []int, k int) {
+	n := len(nums)
+	k = k % n
+
+	reverse(nums)
+	reverse(nums[0:k])
+	reverse(nums[k:])
+}
+
+func reverse(nums []int) {
+	l, r := 0, len(nums)-1
+
+	for l < r {
+		nums[l], nums[r] = nums[r], nums[l]
+		l++
+		r--
+	}
+}
