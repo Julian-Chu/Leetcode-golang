55d0ef4b1ca19a45cb41cd463d669462df2e65c0
Yu-Lang Chu <yulang.chu@gmail.com>
2022-06-24 11:31:20 +0200

    update leetcode34

--- leetcode/Leetcode_34_FindFirstAndLastPositionOfElementInSortedArray/leetcode34.go
+++ leetcode/Leetcode_34_FindFirstAndLastPositionOfElementInSortedArray/leetcode34.go
@@ -1,43 +1,39 @@
 package Leetcode_34_FindFirstAndLastPositionOfElementInSortedArray
 
-//func searchRange(nums []int, target int) []int {
-//	n := len(nums)
-//	if n == 0 {
-//		return []int{-1, -1}
-//	}
-//	l, r := 0, n-1
-//	mid := 0
-//	for l < r {
-//		mid = (l + r) / 2
-//		switch {
-//		case target > nums[mid]:
-//			l = mid + 1
-//		case target < nums[mid]:
-//			r = mid - 1
-//		default:
-//			l, r = mid, mid
-//		}
-//	}
-//	mid = (l + r) / 2
-//	if nums[mid] != target {
-//		return []int{-1, -1}
-//	}
-//
-//	for l = mid; l >= 0; l-- {
-//		if nums[l] != target {
-//			break
-//		}
-//	}
-//	l++
-//	for r = mid; r < n; r++ {
-//		if nums[r] != target {
-//			break
-//		}
-//	}
-//	r--
-//
-//	return []int{l, r}
-//}
+func searchRange_1(nums []int, target int) []int {
+	n := len(nums)
+	if n == 0 {
+		return []int{-1, -1}
+	}
+
+	start, end := 0, n-1
+
+	for start+1 < end {
+		mid := start + (end-start)>>1
+		if nums[mid] >= target {
+			end = mid
+		} else {
+			start = mid
+		}
+	}
+
+	if nums[start] != target && nums[end] != target {
+		return []int{-1, -1}
+	}
+
+	if nums[start] != target {
+		start = end
+	}
+
+	for i := start; i < n; i++ {
+		if nums[i] != target {
+			break
+		}
+		end = i
+	}
+
+	return []int{start, end}
+}
 
 func searchRange(nums []int, target int) []int {
 	return []int{
