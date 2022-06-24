317a965bce9299a4078891b7c3c4267e78b085fb
Yu-Lang Chu <yulang.chu@gmail.com>
2022-06-24 15:26:15 +0200

    add leetcode234

--- leetcode/leetcode234/leetcode234.go
+++ leetcode/leetcode234/leetcode234.go
@@ -33,3 +33,53 @@ func isPalindrome(head *ListNode) bool {
 
 	return true
 }
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func isPalindrome_1(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return true
+	}
+	slow, fast, prev := head, head, head
+
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		prev = slow
+		slow = slow.Next
+	}
+
+	prev.Next = nil
+	cur1 := head
+	cur2 := reverse(slow)
+
+	for cur1 != nil {
+		if cur1.Val != cur2.Val {
+			return false
+		}
+		cur1 = cur1.Next
+		cur2 = cur2.Next
+	}
+
+	return true
+}
+
+func reverse(head *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+
+	dummyHead := &ListNode{}
+
+	for head != nil {
+		next := head.Next
+		head.Next = dummyHead.Next
+		dummyHead.Next = head
+		head = next
+	}
+	return dummyHead.Next
+}
