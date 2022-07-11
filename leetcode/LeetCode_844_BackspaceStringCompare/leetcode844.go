36d1a5bcee698e3d05b5e4b12dd467adcd7cc072
Yu-Lang Chu <yulang.chu@gmail.com>
2022-07-11 18:36:29 +0200

    add leetcode844

--- leetcode/LeetCode_844_BackspaceStringCompare/leetcode844.go
+++ leetcode/LeetCode_844_BackspaceStringCompare/leetcode844.go
@@ -25,3 +25,62 @@ func helper(str string) string {
 	}
 	return string(bs)
 }
+
+func helper_1(s string) string {
+	bs := []byte{}
+	for i := 0; i < len(s); i++ {
+		if s[i] == '#' {
+			if len(bs) > 0 {
+				bs = bs[:len(bs)-1]
+			}
+		} else {
+			bs = append(bs, s[i])
+		}
+	}
+
+	return string(bs)
+}
+
+func backspaceCompare_2(s string, t string) bool {
+	i, j := len(s)-1, len(t)-1
+
+	// #abc, abc
+	for i >= 0 || j >= 0 {
+		skip := 0
+		for i >= 0 {
+			if s[i] == '#' {
+				skip++
+				i--
+			} else if skip > 0 {
+				skip--
+				i--
+			} else {
+				break
+			}
+		}
+
+		skip = 0
+		for j >= 0 {
+			if t[j] == '#' {
+				skip++
+				j--
+			} else if skip > 0 {
+				skip--
+				j--
+			} else {
+				break
+			}
+		}
+
+		if i >= 0 && j >= 0 && s[i] != t[j] {
+			return false
+		}
+		// some ch remains
+		if (i >= 0 && j < 0) || (i < 0 && j >= 0) {
+			return false
+		}
+		i--
+		j--
+	}
+	return true
+}
